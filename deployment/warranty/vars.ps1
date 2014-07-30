20955fc4e1e27c38b39c05037e6fc55e437c5d21
Eduardo Ramos <eduardo@headspring.com>
2014-07-30 13:08:02 -0500

    Fix vars file

--- deployment/warranty/vars.ps1
+++ deployment/warranty/vars.ps1
@@ -7,6 +7,6 @@ $customErrorMode = "RemoteOnly"
 $roundhouse_dir = ".\tools\roundhouse"
 $roundhouse_output_dir = "$roundhouse_dir\output"
 $roundhouse_exe_path = "$roundhouse_dir\rh.exe"
-$roundhouse_version_file = ".\nsb\purchasing\Purchasing.Server.dll"
+$roundhouse_version_file = ".\nsb\warranty\Warranty.Server.dll"
 
 $smtpServer = "SMTPRELAY.CORP.WEEKLEYHOMES.COM"
