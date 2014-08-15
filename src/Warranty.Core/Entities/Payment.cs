cf78359bfd96d0e7ee83788d883031adb71283b2
Deran Schilling <deran@headspring.com>
2014-08-15 18:00:10 -0500

    Add IJdeEntity to Payment

--- src/Warranty.Core/Entities/Payment.cs
+++ src/Warranty.Core/Entities/Payment.cs
@@ -2,17 +2,18 @@ using System;
 
 namespace Warranty.Core.Entities
 {
-    public class Payment : IAuditableEntity
+    public partial class Payment : IAuditableEntity, IJdeEntity
     {
         public Guid PaymentId { get; set; }
         public string VendorNumber { get; set; }
         public decimal Amount { get; set; }
         public string PaymentStatus { get; set; }
         public string JobNumber { get; set; }
-        public string JdeIdentifier { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedDate { get; set; }
         public string UpdatedBy { get; set; }
         public DateTime? UpdatedDate { get; set; }
+
+        public string JdeIdentifier { get; set; }
     }
 }
