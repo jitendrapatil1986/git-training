b2c59e810d877492adb28769634bd9fae7b3f971
Eduardo Ramos <eduardo@headspring.com>
2014-08-18 11:08:54 -0500

    Add object extenions

---
+++ src/Warranty.Core/Configurations/EnumerationDisplayConverter.cs
@@ -0,0 +1,31 @@
+﻿namespace Warranty.Core.Configurations
+{
+    using System;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using Yay.Enumerations;
+
+    public class EnumerationDisplayConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var enumeration = (EnumerationBase)value;
+            writer.WriteValue(enumeration.DisplayName);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.Value == null)
+                return null;
+
+            var displayValue = reader.Value.ToString();
+
+            return EnumerationHelper.GetAll(objectType).SingleOrDefault(x => x.DisplayName == displayValue);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.Closes(typeof(Enumeration<>));
+        }
+    }
+}
---
+++ src/Warranty.Core/Extensions/ObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace Warranty.Core.Extensions
+{
+    using System.IO;
+    using Configurations;
+    using Newtonsoft.Json;
+
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj, bool useEnumerationDisplay = false)
+        {
+            var jsonSerializer =
+                JsonSerializer.Create(new JsonSerializerSettings
+                {
+                    ContractResolver = new WarrantyContractResolver(),
+                });
+
+            if (useEnumerationDisplay)
+            {
+                jsonSerializer.Converters.Add(new EnumerationDisplayConverter());
+            }
+
+            var stringWriter = new StringWriter();
+            jsonSerializer.Serialize(stringWriter, obj);
+
+            return stringWriter.ToString();
+        }
+
+    }
+}
