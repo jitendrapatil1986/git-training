22b93805f2de0b785908260052d924164a9ab744
Dane Schilling <dane.schilling@headspring.com>
2014-08-25 14:21:16 -0500

    Add scripts to add tables

---
+++ src/Database/Warranty/Up/0048_AddJobStagesTable.sql
@@ -0,0 +1,13 @@
+CREATE TABLE JobStages (
+    JobStageId INT IDENTITY(1,1),
+    JobId UNIQUEIDENTIFIER,
+    JobStage INT,
+    CompletionDate DATE,
+    JdeIdentifier VARCHAR(25),
+	CreatedDate datetime2(7) NULL,
+	CreatedBy varchar(255) NULL,
+	UpdatedDate datetime2(7) NULL,
+	UpdatedBy varchar(255) NULL,
+    CONSTRAINT PK_JobStages
+        PRIMARY KEY (JobStageId)
+)
