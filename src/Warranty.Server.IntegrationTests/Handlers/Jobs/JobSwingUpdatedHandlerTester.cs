2c30053c749f50c5388ee543f7fa5c2c97834716
Dane Schilling <dane.schilling@headspring.com>
2014-08-26 12:09:11 -0500

    Add swing updated handler and test

---
+++ src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobSwingUpdatedHandlerTester.cs
@@ -0,0 +1,32 @@
+﻿using Accounting.Events.Job;
+using NUnit.Framework;
+using Should;
+using Warranty.Core.Entities;
+
+namespace Warranty.Server.IntegrationTests.Handlers.Jobs
+{
+    [TestFixture]
+    public class JobSwingUpdatedHandlerTester : HandlerTester<JobSwingUpdated>
+    {
+        private Job _job;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _job = GetSaved<Job>();
+
+            Send(x =>
+            {
+                x.JDEId = _job.JdeIdentifier;
+                x.Swing = "N";
+            });
+        }
+
+        [Test]
+        public void Job_Swing_Should_Be_Updated()
+        {
+            var payment = Get<Job>(_job.JobId);
+            payment.Swing.ShouldEqual(Event.Swing);
+        }
+    }
+}
