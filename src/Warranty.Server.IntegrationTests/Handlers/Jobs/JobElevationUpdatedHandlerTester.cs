8fb8c25b209e68863464691a85f2730169f7c2fe
Dane Schilling <dane.schilling@headspring.com>
2014-08-26 13:48:52 -0500

    Add Job Elevation Updated handler and tests

---
+++ src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobElevationUpdatedHandlerTester.cs
@@ -0,0 +1,32 @@
+﻿using Accounting.Events.Job;
+using NUnit.Framework;
+using Should;
+using Warranty.Core.Entities;
+
+namespace Warranty.Server.IntegrationTests.Handlers.Jobs
+{
+    [TestFixture]
+    public class JobElevationUpdatedHandlerTester : HandlerTester<JobElevationUpdated>
+    {
+        private Job _job;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _job = GetSaved<Job>();
+
+            Send(x =>
+            {
+                x.JDEId = _job.JdeIdentifier;
+                x.Elevation = "S";
+            });
+        }
+
+        [Test]
+        public void Job_Elevation_Should_Be_Updated()
+        {
+            var job = Get<Job>(_job.JobId);
+            job.Elevation.ShouldEqual(Event.Elevation);
+        }
+    }
+}
---
+++ src/Warranty.Server/Handlers/Jobs/JobElevationUpdatedHandler.cs
@@ -0,0 +1,29 @@
+﻿using Accounting.Events.Job;
+using NPoco;
+using NServiceBus;
+using Warranty.Core.Entities;
+using Warranty.Server.Extensions;
+
+namespace Warranty.Server.Handlers.Jobs
+{
+    public class JobElevationUpdatedHandler : IHandleMessages<JobElevationUpdated>
+    {
+        private readonly IDatabase _database;
+
+        public JobElevationUpdatedHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Handle(JobElevationUpdated message)
+        {
+            using (_database)
+            {
+                var job = _database.SingleByJdeId<Job>(message.JDEId);
+
+                job.Elevation = message.Elevation;
+                _database.Update(job);
+            }
+        }
+    }
+}
