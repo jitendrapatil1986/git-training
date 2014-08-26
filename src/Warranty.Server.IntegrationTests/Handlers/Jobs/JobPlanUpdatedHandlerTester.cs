d47cbecfc2aefefcc8a40c300ea1e8d20025705d
Dane Schilling <dane.schilling@headspring.com>
2014-08-26 12:56:35 -0500

    Add plan updated handler and test

---
+++ src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobPlanUpdatedHandlerTester.cs
@@ -0,0 +1,32 @@
+﻿using Accounting.Events.Job;
+using NUnit.Framework;
+using Should;
+using Warranty.Core.Entities;
+
+namespace Warranty.Server.IntegrationTests.Handlers.Jobs
+{
+    [TestFixture]
+    public class JobPlanUpdatedHandlerTester : HandlerTester<JobPlanUpdated>
+    {
+        private Job _job;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _job = GetSaved<Job>();
+
+            Send(x =>
+            {
+                x.JDEId = _job.JdeIdentifier;
+                x.Plan = "1234";
+            });
+        }
+
+        [Test]
+        public void Job_Close_Date_Should_Be_Updated()
+        {
+            var payment = Get<Job>(_job.JobId);
+            payment.PlanNumber.ShouldEqual(Event.Plan);
+        }
+    }
+}
