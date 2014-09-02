a506a4b63433687941cd563e7b4be02d110386ef
Dane Schilling <dane.schilling@headspring.com>
2014-09-01 19:41:56 -0500

    Add home owner name updated

---
+++ src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobHomeBuyerNameUpdatedHandlerTester.cs
@@ -0,0 +1,40 @@
+﻿using Accounting.Events.Job;
+using NUnit.Framework;
+using Should;
+using Warranty.Core.Entities;
+
+namespace Warranty.Server.IntegrationTests.Handlers.Jobs
+{
+    [TestFixture]
+    public class JobHomeBuyerNameUpdatedHandlerTester : HandlerTester<JobHomeBuyerNameUpdated>
+    {
+        private HomeOwner _homeOwner;
+        private Job _job;
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            _job = GetSaved<Job>();
+            _homeOwner = GetSaved<HomeOwner>(ho => ho.JobId = _job.JobId);
+
+            _job.CurrentHomeOwnerId = _homeOwner.HomeOwnerId;
+
+            using (TestDatabase)
+                TestDatabase.Update(_job);
+
+            Send(x =>
+            {
+                x.JDEId = _job.JdeIdentifier;
+                x.BuyerName = "Jo Test";
+            });
+        }
+
+        [Test]
+        public void Job_Close_Date_Should_Be_Updated()
+        {
+            var job = Get<Job>(_job.JobId);
+            var homeOwner = Get<HomeOwner>(job.CurrentHomeOwnerId);
+            homeOwner.HomeOwnerName.ShouldEqual(Event.BuyerName);
+        }
+    }
+}
---
+++ src/Warranty.Server/Handlers/Jobs/JobHomeBuyerNameUpdatedHandler.cs
@@ -0,0 +1,30 @@
+using Accounting.Events.Job;
+using NPoco;
+using NServiceBus;
+using Warranty.Core.Entities;
+using Warranty.Server.Extensions;
+
+namespace Warranty.Server.Handlers.Jobs
+{
+    public class JobHomeBuyerNameUpdatedHandler : IHandleMessages<JobHomeBuyerNameUpdated>
+    {
+        private readonly IDatabase _database;
+
+        public JobHomeBuyerNameUpdatedHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Handle(JobHomeBuyerNameUpdated message)
+        {
+            using (_database)
+            {
+                var job = _database.SingleByJdeId<Job>(message.JDEId);
+                var homeOwner = _database.SingleById<HomeOwner>(job.CurrentHomeOwnerId);
+
+                homeOwner.HomeOwnerName = message.BuyerName;
+                _database.Update(homeOwner);
+            }
+        }
+    }
+}
