96e988716254ba4f53cc9c14a1dc1b1be13da355
Deran Schilling <deran@headspring.com>
2014-09-02 14:16:37 -0500

    Add JS for ManageLookups

---
+++ src/Warranty.UI/Scripts/app/admin/ManageLookups.js
@@ -0,0 +1,25 @@
+﻿require(['/Scripts/app/main.js'], function () {
+    require(['modelData', 'jquery', 'ko', 'urls'], function (modelData, $, ko, urls) {
+        function manageLookupsViewModel() {
+            var self = this;
+            self.availableLookups = ko.observableArray(modelData.availableLookups);
+            self.selectedLookup = ko.observable('');
+
+            self.lookupType = ko.computed(function() {
+                return this.selectedLookup;
+            });
+            self.lookupItems = ko.observableArray([]);
+
+            self.removeItem = function (item) {
+                self.lookupItems.remove(item);
+            };
+
+            self.selectionChanged = function () {
+                //self.add$.get(urls.)
+            };
+        }
+
+        var viewModel = new manageLookupsViewModel();
+        ko.applyBindings(viewModel);
+    });
+});
