6f07bd750decb0041a280bd2281f754cf5089e51
Eduardo Ramos <eduardo@headspring.com>
2014-09-15 13:01:54 -0500

    Create ToDo to map wsr to a community

---
+++ src/Warranty.Core/Features/CommunityEmployeeAssignment/CommunityEmployeAssignmentCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Warranty.Core.Features.CommunityEmployeeAssignment
+{
+    public class CommunityEmployeAssignmentCommand : ICommand<bool>
+    {
+        public Guid CommunityId { get; set; }
+        public string Employeenumber { get; set; }
+    }
+}
