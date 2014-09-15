73e05f207e98985b4e0957b1b2109c48899a4870
Eduardo Ramos <eduardo@headspring.com>
2014-09-15 18:17:26 -0500

    Refactored code

--- src/Warranty.UI/Scripts/app/Home/widgetToDos.js
+++ src/Warranty.UI/Scripts/app/Home/widgetToDos.js
@@ -51,9 +51,8 @@ define(['urls','jquery'], function(urls, $) {
                 url: url,
                 data: { CommunityId: communityId, EmployeeNumber: employeeNumber },
                 success: function (result) {
-                    debugger;
                     if (result.success == "True") {
-                        var divToHide = button.parent().parent().parent();
+                        var divToHide = button.parents('div[class^="todo"]').eq(-2);
                         divToHide.addClass('hide');
                     }
                 }
