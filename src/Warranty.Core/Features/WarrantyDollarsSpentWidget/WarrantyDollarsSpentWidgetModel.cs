d09522c52ccee65537446ba0c00db318a7475e1c
Deran Schilling <deran@headspring.com>
2014-09-19 21:01:04 -0500

    Warranty Dollars Spent Widget

---
+++ src/Warranty.Core/Features/WarrantyDollarsSpentWidget/WarrantyDollarsSpentWidgetModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Warranty.Core.Features.WarrantyDollarsSpentWidget
+{
+    public class WarrantyDollarsSpentWidgetModel
+    {
+        public decimal AmountSpent { get; set; }
+        public decimal AmountSpentLastMonth { get; set; }
+    }
+}
