b05b4ad553fd624bb80d2f380fab2bfe29dc1633
Kiet Huynh <kiet.huynh@headspring.com>
2014-09-24 16:23:58 -0500

    BS-4798 - Add 'Homeowner Verification Signature' functionality to Service Calls.

---
+++ src/Warranty.Core/Features/EditServiceCallStatus/VerifyHomeownerSignatureServiceCallStatusCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿namespace Warranty.Core.Features.EditServiceCallStatus
+{
+    using Entities;
+    using Enumerations;
+    using NPoco;
+
+    public class VerifyHomeownerSignatureServiceCallStatusCommandHandler : ICommandHandler<VerifyHomeownerSignatureServiceCallStatusCommand, VerifyHomeownerSignatureServiceCallStatusModel>
+    {
+        private readonly IDatabase _database;
+
+        public VerifyHomeownerSignatureServiceCallStatusCommandHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public VerifyHomeownerSignatureServiceCallStatusModel Handle(VerifyHomeownerSignatureServiceCallStatusCommand message)
+        {
+            using (_database)
+            {
+                var updateServiceCall = _database.SingleById<ServiceCall>(message.ServiceCallId);
+                updateServiceCall.HomeownerVerificationSignature = message.HomeownerVerificationSignature;
+                updateServiceCall.HomeownerVerificationSignatureDate = message.HomeownerVerificationSignatureDate;
+
+                if (updateServiceCall.ServiceCallStatus != ServiceCallStatus.Closed)
+                    updateServiceCall.ServiceCallStatus = ServiceCallStatus.HomeownerSigned;
+
+                _database.Update(updateServiceCall);
+
+                var model = new VerifyHomeownerSignatureServiceCallStatusModel
+                    {
+                        ServiceCallId = updateServiceCall.ServiceCallId,
+                        ServiceCallStatus = updateServiceCall.ServiceCallStatus,
+                        HomeownerVerificationSignature = updateServiceCall.HomeownerVerificationSignature,
+                        HomeownerVerificationSignatureDate = updateServiceCall.HomeownerVerificationSignatureDate,
+                    };
+
+                return model;
+            }
+        }
+    }
+}
