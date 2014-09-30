e9bc69f0b592bee8e5a017ab2f9e5254290caa5c
Eduardo Ramos <eduardo@headspring.com>
2014-09-29 20:34:10 -0500

    Send email to WSC when SC is escalated

--- src/Warranty.Core/Features/ServiceCallToggleActions/ServiceCallToggleEscalateCommand.cs
+++ src/Warranty.Core/Features/ServiceCallToggleActions/ServiceCallToggleEscalateCommand.cs
@@ -2,7 +2,7 @@
 
 namespace Warranty.Core.Features.ServiceCallToggleActions
 {
-    public class ServiceCallToggleEscalateCommand : ICommand
+    public class ServiceCallToggleEscalateCommand : ICommand<ServiceCallToggleEscalateCommandResult>
     {
         public Guid ServiceCallId { get; set; }
         public string Text { get; set; }
