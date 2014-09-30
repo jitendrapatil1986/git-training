65baf2a8076345cc53a506e3b0901c59bfddaaa1
Deran Schilling <deran@headspring.com>
2014-09-29 23:22:33 -0500

    Initial landing page for stats

--- src/Warranty.Core/Features/ServiceCallStats/ServiceCallStatsModel.cs
+++ src/Warranty.Core/Features/ServiceCallStats/ServiceCallStatsModel.cs
@@ -1,6 +1,23 @@
 ﻿namespace Warranty.Core.Features.ServiceCallStats
 {
+    using System;
+    using System.Collections.Generic;
+
     public class ServiceCallStatsModel
     {
+        public IEnumerable<LineItem> LineItems { get; set; }
+
+        public class LineItem
+        {
+            public int AverageDaysClosed { get; set; }
+            public decimal PercentClosedWithinSevenDays { get; set; }
+            public decimal TotalDollarsSpent { get; set; }
+            public int NumberOfWarrantableHomes { get; set; }
+            public decimal DollarsSpentPerHome { get; set; }
+            public Guid EmployeeId { get; set; }
+            public string EmployeeName { get; set; }
+            public string EmployeeNumber { get; set; }
+            public string CityCode { get; set; }            
+        }
     }
 }
