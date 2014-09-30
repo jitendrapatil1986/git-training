5e784ef010aff76e92d17d29a9152dff178e90b5
Deran Schilling <deran@headspring.com>
2014-09-29 22:08:35 -0500

    Add context to "Add" links in header of call summary

--- src/Warranty.UI/Core/Helpers/Format.cs
+++ src/Warranty.UI/Core/Helpers/Format.cs
@@ -365,7 +365,7 @@ namespace Warranty.UI.Core.Helpers
         public static MvcHtmlString EditablePhoneNumber(PhoneNumberType phoneNumberType, string phoneNumber, string postUrl, Guid homeownerId)
         {
             var htmlString =
-                string.Format("<div class='inline-editable-text'><a class='glyphicon glyphicon-{0} text-muted visible-xs' href='tel:{4}'></a><span class='glyphicon glyphicon-{0} text-muted hidden-xs'></span> <a href='#' class='hidden-xs phone-number-with-extension' data-type='text' data-emptytext='Add' id='{1}_Phone' data-url='{2}' data-pk='{3}' data-clear='false' data-value='{4}'>{4}</a></div>", phoneNumberType.Icon, phoneNumberType.DisplayName, postUrl, homeownerId, phoneNumber);
+                string.Format("<div class='inline-editable-text'><a class='glyphicon glyphicon-{0} text-muted visible-xs' href='tel:{4}'></a><span class='glyphicon glyphicon-{0} text-muted hidden-xs'></span> <a href='#' class='hidden-xs phone-number-with-extension' data-type='text' data-emptytext='Add {1}' id='{1}_Phone' data-url='{2}' data-pk='{3}' data-clear='false' data-value='{4}'>{4}</a></div>", phoneNumberType.Icon, phoneNumberType.DisplayName, postUrl, homeownerId, phoneNumber);
 
             return MvcHtmlString.Create(htmlString);
         }
@@ -375,7 +375,7 @@ namespace Warranty.UI.Core.Helpers
             return
                 MvcHtmlString.Create(
                     string.Format(@"<div class='inline-editable-text'><a class='glyphicon glyphicon-envelope text-muted' href='mailto:{2}?subject=David Weekley Homes Warranty Service Call %23 {3}'></a> 
-                                    <a href='mailto:{2}?subject=David Weekley Homes Warranty Service Call %23 {3}' class='hidden-xs' data-type='text' data-emptytext='Add' id='Email' data-url='{0}' data-pk='{1}' data-clear='false' data-value='{2}'>{2}</a></div>", postUrl, homeownerId, email, callNumber));
+                                    <a href='mailto:{2}?subject=David Weekley Homes Warranty Service Call %23 {3}' class='hidden-xs' data-type='text' data-emptytext='Add Email' id='Email' data-url='{0}' data-pk='{1}' data-clear='false' data-value='{2}'>{2}</a></div>", postUrl, homeownerId, email, callNumber));
         }
 
         public static MvcHtmlString EditableEmailForJob(string email, string postUrl, Guid homeownerId)
