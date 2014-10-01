dc3fa5258abed264b8bb2705053bab4e9a522ade
Eduardo Ramos <eduardo@headspring.com>
2014-10-01 08:21:42 -0500

    Initial infrastructure for FileManagement

---
+++ src/Warranty.Core/FileManagement/FileSystemManager.cs
@@ -0,0 +1,100 @@
+﻿namespace Warranty.Core.FileManagement
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.IO;
+
+    public class FileSystemManager<TEntity> : IFileSystemManager<TEntity>
+    {
+        private readonly ITemporaryFileUploadsService _temporaryFileUploadsService;
+        private readonly string _fileAttachmentsRootPath;
+
+        public string EntityName { get; set; }
+
+        public FileSystemManager(ITemporaryFileUploadsService temporaryFileUploadsService)
+        {
+            _temporaryFileUploadsService = temporaryFileUploadsService;
+            _fileAttachmentsRootPath = ConfigurationManager.AppSettings["DocumentSharePath"];
+            EntityName = typeof(TEntity).Name;
+        }
+
+        public string GetCollisionAndLengthSafeFileName(string path, string fileName)
+        {
+            const int maxLengthOfFile = 40;
+
+            if (fileName.Length > maxLengthOfFile)
+            {
+                var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+                var fileNameExtension = Path.GetExtension(fileName);
+
+                var truncatedFileNameLength = maxLengthOfFile - fileNameExtension.Length;
+                var truncatedFileName = fileNameWithoutExtension.Substring(0, truncatedFileNameLength) + fileNameExtension;
+                fileName = truncatedFileName;
+            }
+
+            var filePath = Path.Combine(path, fileName);
+            var file = new FileInfo(filePath);
+
+            var counter = 1;
+            while (file.Exists)
+            {
+                var newFileName = Path.GetFileNameWithoutExtension(fileName);
+                newFileName += "_" + (counter++) + Path.GetExtension(fileName);
+
+                filePath = Path.Combine(path, newFileName);
+                file = new FileInfo(filePath);
+            }
+
+            return file.Name;
+        }
+
+        public List<string> MoveFilesToDirectoryAndRenameToAvoidCollisions(TEntity entity, IEnumerable<string> temporaryFileIds, string newFriendlyName = null)
+        {
+            var destinationFilePath = Path.Combine(_fileAttachmentsRootPath, EntityName);
+            return MoveFilesToDirectoryAndRenameToAvoidCollisions(destinationFilePath, temporaryFileIds, newFriendlyName);
+        }
+
+        public List<string> MoveFilesToDirectoryAndRenameToAvoidCollisions(IEnumerable<string> temporaryFileIds, string newFriendlyName = null)
+        {
+            var destinationFilePath = Path.Combine(_fileAttachmentsRootPath, EntityName);
+            return MoveFilesToDirectoryAndRenameToAvoidCollisions(destinationFilePath, temporaryFileIds, newFriendlyName);
+        }
+
+        private List<string> MoveFilesToDirectoryAndRenameToAvoidCollisions(string destinationFilePath, IEnumerable<string> temporaryFileIds, string newFriendlyName = null)
+        {
+            if (!Directory.Exists(destinationFilePath))
+                Directory.CreateDirectory(destinationFilePath);
+
+            var correctedFileNames = new List<string>();
+            foreach (var fileIds in temporaryFileIds)
+            {
+                var originalFileName = _temporaryFileUploadsService.GetOriginalFileName(fileIds);
+                var newFileName = originalFileName;
+
+                if (!string.IsNullOrWhiteSpace(newFriendlyName))
+                {
+                    newFileName = string.Format("{0}{1}", newFriendlyName, Path.GetExtension(originalFileName));
+                }
+
+                var collisionSafeScholarFileName = GetCollisionAndLengthSafeFileName(destinationFilePath, newFileName);
+                correctedFileNames.Add(collisionSafeScholarFileName);
+
+                var temporaryFile = _temporaryFileUploadsService.GetTemporaryFile(fileIds);
+                var fullyQualifiedFilePathAndName = Path.Combine(destinationFilePath, collisionSafeScholarFileName);
+                temporaryFile.MoveTo(fullyQualifiedFilePathAndName);
+            }
+
+            return correctedFileNames;
+        }
+
+        public void DeleteAttachment(TEntity entity, string fileName)
+        {
+            var filePath = Path.Combine(_fileAttachmentsRootPath, EntityName, fileName);
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
---
+++ src/Warranty.UI/Core/ModelBinders/FileUploadsPropertyBinder.cs
@@ -0,0 +1,163 @@
+﻿namespace Warranty.UI.Core.ModelBinders
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Web.Mvc;
+    using Helpers;
+    using Warranty.Core.FileManagement;
+    using Warranty.Core.Services;
+    using Yay.Enumerations;
+    using Yay.Enumerations.MVC.ModelBinders;
+
+
+    public class FileUploadsPropertyBinder : FilteredPropertyBinderBase
+    {
+        public const string FileUploaderIdFieldName = "FileUploaderId";
+
+        private readonly TemporaryFileUploadsService _temporaryFileUploadsService;
+
+        public FileUploadsPropertyBinder(TemporaryFileUploadsService temporaryFileUploadsService)
+        {
+            _temporaryFileUploadsService = temporaryFileUploadsService;
+        }
+
+        public override bool ShouldBind(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
+        {
+            return propertyDescriptor.PropertyType.IsAssignableTo<FileUpload>();
+        }
+
+        public override object GetPropertyValue(ControllerContext controllerContext, ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
+        {
+            try
+            {
+                var uploaderId = GetChildPropertyAttemptedValue(bindingContext, propertyDescriptor, FileUploaderIdFieldName);
+                var rawNewUploads = GetChildPropertyAttemptedValue(bindingContext, propertyDescriptor, uploaderId);
+                var rawPendingUploads = GetChildPropertyAttemptedValue(bindingContext, propertyDescriptor, x => x.RawPendingUploads);
+
+                var allPendingUploads = JoinAllUploads(rawPendingUploads, rawNewUploads);
+
+                var fileGuids = GetFileGuids(allPendingUploads);
+
+                var temporaryFileUploadInfos = new List<TemporaryFileUploadInfo>();
+                foreach (var fileGuid in fileGuids)
+                {
+                    var temporaryFileUploadInfo = new TemporaryFileUploadInfo
+                    {
+                        FileId = fileGuid,
+                        FileName = _temporaryFileUploadsService.GetOriginalFileName(fileGuid),
+                    };
+
+                    temporaryFileUploadInfos.Add(temporaryFileUploadInfo);
+                }
+
+                var previousFiles = ReHydrateContractFileInfos(bindingContext, propertyDescriptor);
+
+                var result = new FileUpload
+                {
+                    RawPendingUploads = allPendingUploads,
+                    PendingFiles = temporaryFileUploadInfos,
+                    PendingFileIds = fileGuids,
+                    PreviousFiles = previousFiles.ToArray(),
+                };
+
+                PerformModelValidation(bindingContext, propertyDescriptor, result);
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("Unable to locate a valid value for '{0}'", GetRootPropertyName(bindingContext, propertyDescriptor));
+                throw new ApplicationException(message, ex);
+            }
+        }
+
+        private static List<string> ReHydrateContractFileInfos(ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
+        {
+            var rootPropertyName = GetRootPropertyName(bindingContext, propertyDescriptor);
+
+            var previousFiles = new List<string>();
+            for (var i = 0; i < 1000; i++)
+            {
+                var rootPropertyPreviousFileN = string.Format("{0}.{1}[{2}]",
+                    rootPropertyName,
+                    ReflectionService.GetPropertyName<FileUpload>(x => x.PreviousFiles),
+                    i);
+
+                var fileIdValueProviderResult = bindingContext.ValueProvider.GetValue(rootPropertyPreviousFileN);
+                if (fileIdValueProviderResult == null)
+                    break;
+
+                var fileName = bindingContext.ValueProvider.GetValue(rootPropertyPreviousFileN).AttemptedValue;
+
+                previousFiles.Add(fileName);
+            }
+            return previousFiles;
+        }
+
+        private static void PerformModelValidation(ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, FileUpload result)
+        {
+            var attributes = propertyDescriptor.Attributes;
+            if (attributes.OfType<RequiredAttribute>().Any() && !result.PendingFileIds.Any())
+            {
+                var requiredAttribute = attributes.OfType<RequiredAttribute>().First();
+                bindingContext.ModelState.AddModelError(GetRootPropertyName(bindingContext, propertyDescriptor), requiredAttribute.ErrorMessage);
+            }
+        }
+
+        private static string JoinAllUploads(string rawPreviousUploads, string rawNewUploads)
+        {
+            if (string.IsNullOrWhiteSpace(rawPreviousUploads))
+                return rawNewUploads;
+
+            if (string.IsNullOrWhiteSpace(rawNewUploads))
+                return rawPreviousUploads;
+
+            return rawPreviousUploads + "/" + rawNewUploads;
+        }
+
+        private static List<string> GetFileGuids(string raw)
+        {
+            return string.IsNullOrWhiteSpace(raw) ? new List<string>() : raw.Split('/').ToList();
+        }
+
+        protected static string GetChildPropertyAttemptedValue(ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, Expression<Func<FileUpload, object>> expression)
+        {
+            var propertyName = ReflectionService.GetPropertyName(expression);
+
+            return GetChildPropertyAttemptedValue(bindingContext, propertyDescriptor, propertyName);
+        }
+
+        protected static string GetChildPropertyAttemptedValue(ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor, string propertyName)
+        {
+            var rootPropertyName = GetRootPropertyName(bindingContext, propertyDescriptor);
+
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(rootPropertyName + "." + propertyName);
+
+            if (valueProviderResult == null && PropertyIsInACollection(rootPropertyName))
+                valueProviderResult = bindingContext.ValueProvider.GetValue(ModelBindingHelper.ReplaceSpecialChactersWithDotReplacement(rootPropertyName) + "." + propertyName);
+
+            return valueProviderResult != null ? valueProviderResult.AttemptedValue : null;
+        }
+
+        private static bool PropertyIsInACollection(string rootPropertyName)
+        {
+            var isCollection = new System.Text.RegularExpressions.Regex(@"\[\w\]");
+            return isCollection.IsMatch(rootPropertyName);
+        }
+
+        private static string GetRootPropertyName(ModelBindingContext bindingContext, PropertyDescriptor propertyDescriptor)
+        {
+            var prefix = "";
+
+            if (!string.IsNullOrWhiteSpace(bindingContext.ModelName))
+                prefix = bindingContext.ModelName + ".";
+
+            var rootPropertyName = prefix + propertyDescriptor.Name;
+            return rootPropertyName;
+        }
+    }
+}
