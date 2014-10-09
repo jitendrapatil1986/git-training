838a30a8ca7c899f634d7a9170c7c5174a97a3ee
Kiet Huynh <kiet.huynh@headspring.com>
2014-10-09 16:17:58 -0500

    BS-4987 - Create 'WSR Loading Report'.

---
+++ src/Warranty.UI/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Warranty.UI.Controllers
+{
+    using System.Web.Mvc;
+    using Warranty.Core;
+    using Warranty.Core.Features.Report.WSRLoadingReport;
+
+    public class ReportController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ReportController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public ActionResult WSRLoadingReport()
+        {
+            var resultModel = _mediator.Request(new WSRLoadingReportQuery());
+
+            return View(resultModel);
+        }
+
+        [HttpPost]
+        public ActionResult WSRLoadingReport(WSRLoadingReportModel model)
+        {
+            var resultModel = _mediator.Request(new WSRLoadingReportQuery { queryModel = model });
+
+            return View(resultModel);
+        }
+    }
+}
