104068d356479a1849ae7f18567a39e547995cc1
Kiet Huynh <kiet.huynh@headspring.com>
2014-10-22 10:33:50 -0500

    BS-5101 - Create 'Homeowner Changed' event.

---
+++ src/Warranty.Events/JobHomeownerChanged.cs
@@ -0,0 +1,31 @@
+﻿namespace Warranty.Events
+{
+    using System;
+    using System.Collections.Generic;
+    using NServiceBus;
+
+    public class JobHomeownerChanged : IEvent
+    {
+        public Guid JobId { get; set; }
+        public int? HomeownerNumber { get; set; }
+        public string HomeownerName { get; set; }
+        public string HomeownerHomePhone { get; set; }
+        public string HomeownerEmailAddress { get; set; }
+
+        public List<AdditionalPhoneContact> AdditionalPhoneContacts { get; set; }
+
+        public class AdditionalPhoneContact
+        {
+            public string ContactType { get; set; }
+            public string ContactValue { get; set; }
+        }
+
+        public List<AdditionalEmailContact> AdditionalEmailContacts { get; set; }
+
+        public class AdditionalEmailContact
+        {
+            public string ContactType { get; set; }
+            public string ContactValue { get; set; }
+        }
+    }
+}
---
+++ src/Warranty.Server/Handlers/Jobs/JobHomeownerChangedHandler.cs
@@ -0,0 +1,49 @@
+﻿namespace Warranty.Server.Handlers.Jobs
+{
+    using System.Linq;
+    using Core.Entities;
+    using Core.Enumerations;
+    using Events;
+    using InnerMessages;
+    using NPoco;
+    using NServiceBus;
+
+    public class JobHomeownerChangedHandler : IHandleMessages<NotifyJobHomeownerChanged>
+    {
+        private readonly IBus _bus;
+        private readonly IDatabase _database;
+
+        public JobHomeownerChangedHandler(IBus bus, IDatabase database )
+        {
+            _bus = bus;
+            _database = database;
+        }
+        public void Handle(NotifyJobHomeownerChanged message)
+        {
+            using (_database)
+            {
+                var job = _database.SingleById<Job>(message.JobId);
+                var homeowner = _database.SingleById<HomeOwner>(job.CurrentHomeOwnerId);
+                var homeownerContacts = _database.Fetch<HomeownerContact>().Where(x => x.HomeownerId == homeowner.HomeOwnerId);
+
+                _bus.Publish<JobHomeownerChanged>(x =>
+                    {
+                        x.JobId = job.JobId;
+                        x.HomeownerName = homeowner.HomeOwnerName;
+                        x.HomeownerHomePhone = homeowner.HomePhone;
+                        x.HomeownerEmailAddress = homeowner.EmailAddress;
+                        x.AdditionalPhoneContacts = homeownerContacts.Select(y => new JobHomeownerChanged.AdditionalPhoneContact
+                            {
+                                ContactType = HomeownerContactType.Phone.DisplayName,
+                                ContactValue = y.ContactValue,
+                            }).ToList();
+                        x.AdditionalEmailContacts = homeownerContacts.Select(y => new JobHomeownerChanged.AdditionalEmailContact
+                            {
+                                ContactType = HomeownerContactType.Email.DisplayName,
+                                ContactValue = y.ContactValue,
+                            }).ToList();
+                    });
+            }
+        }
+    }
+}
