bd2ba29f6638afe5efaaf88bda1de22e76a2690f
Kiet Huynh <kiet.huynh@headspring.com>
2014-11-03 09:43:07 -0600

    Remove classes not needed for searching employees b/c the model already passes the employee list to the form.

--- src/Warranty.Core/Features/QuickSearch/QuickSearchEmployeesQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Warranty.Core.Features.QuickSearch
-{
-    using System.Collections.Generic;
-
-    public class QuickSearchEmployeesQuery : IQuery<IEnumerable<QuickSearchEmployeeModel>>
-    {
-        public string Query { get; set; }
-    }
-}
