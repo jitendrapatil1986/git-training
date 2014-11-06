c86695b6cb5989b43632051af193f73c40f3fca3
Adam Hopkins <adam.hopkins@headspring.com>
2014-11-06 16:21:41 -0600

    backcharge response handler

---
+++ src/Warranty.Server/Handlers/Payments/RequestBackchargeResponseHandler.cs
@@ -0,0 +1,31 @@
+﻿namespace Warranty.Server.Handlers.Payments
+{
+    using Accounting.Events.Backcharge;
+    using Core.Entities;
+    using Core.Enumerations;
+    using NPoco;
+    using NServiceBus;
+
+    public class RequestBackchargeResponseHandler : IHandleMessages<RequestBackchargeResponse>
+    {
+        private readonly IDatabase _database;
+
+        public RequestBackchargeResponseHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Handle(RequestBackchargeResponse message)
+        {
+            using (_database)
+            {
+                var backcharge = _database.SingleOrDefaultById<Backcharge>(message.BackchargeId);
+                if (backcharge != null)
+                {
+                    backcharge.BackchargeStatus = BackchargeStatus.Pending;
+                    _database.Update(backcharge);
+                }
+            }
+        }
+    }
+}
