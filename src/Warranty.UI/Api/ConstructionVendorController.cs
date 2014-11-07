974b375b3d65c6796b19c620692f618f67429868
Adam Hopkins <adam.hopkins@headspring.com>
2014-11-07 13:54:48 -0600

    wip: construction vendors on line items page

---
+++ src/Warranty.UI/Api/ConstructionVendorController.cs
@@ -0,0 +1,23 @@
+﻿namespace Warranty.UI.Api
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Warranty.Core;
+    using Warranty.Core.Features.ServiceCallSummary.ServiceCallLineItem;
+
+    public class ConstructionVendorController : ApiController
+    {
+        private readonly IMediator _mediator;
+
+        public ConstructionVendorController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public IEnumerable<ConstructionVendorModel> ConstructionVendors(string jobNumber, string costCode)
+        {
+            return _mediator.Request<IEnumerable<ConstructionVendorModel>>(new ConstructionVendorQuery() { JobNumber = jobNumber, CostCode = costCode });
+        }
+    }
+}
