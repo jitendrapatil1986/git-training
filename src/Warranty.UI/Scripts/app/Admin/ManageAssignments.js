4f64243c141b7b688fc7efdbadd67bd45d7ffaea
Kiet Huynh <kiet.huynh@headspring.com>
2014-11-10 15:55:06 -0600

    Update to use knockout custom binding for twitter typeahead.  Also refactor view model and html.

--- src/Warranty.UI/Scripts/app/Admin/ManageAssignments.js
+++ src/Warranty.UI/Scripts/app/Admin/ManageAssignments.js
@@ -12,13 +12,15 @@
                 self.communityName = options.name;
                 self.communityAssignmentId = ko.observable('');
                 self.communityAssignedEmployees = ko.observableArray([]);
-                self.selectedEmployeeId = ko.observable('');
-                self.selectedEmployeeName = ko.observable('');
-
+                self.employeeId = ko.observable('');
+                self.employeeName = ko.observable('');
+                self.typeaheadSelectedEmployeeId = ko.observable('');
+                self.typeaheadSelectedEmployeeName = ko.observable('');
+                
                 //employees is the submodel, which is employee list tied to the community, where a community only has 1 employee assigned to it.
                 _(options.employees).each(function (item) {
                     self.communityAssignmentId(item.employeeAssignmentId);
-                    self.selectedEmployeeName(item.name);
+                    self.employeeName(item.name);
                 });
 
                 self.assignEmployeeToCommunity = function () {
@@ -27,7 +29,7 @@
                         url: urls.AssignWSR.UpdateAssignment,
                         data: {
                             selectedCommunityId: this.communityId,
-                            selectedEmployeeId: this.selectedEmployeeId()
+                            selectedEmployeeId: this.employeeId()
                         },
                         dataType: "json"
                     }).fail(function () {
@@ -47,16 +49,85 @@
                     this.assigneeEditing(true);
                     $(event.currentTarget).closest(".communityDesc").find(".typeahead").focus();
                 };
+                
+                //specify typeahead options for knockout custom binding.
+                self.typeaheadOptions = {
+                    name: 'employees',
+                    displayKey: 'employeeName',
+                    limit: 20,
+                    minLength: 2,
+                    remote: {
+                        url: urls.QuickSearch.Employees + '?query=%QUERY',
+                        filter: function (list) {
+                            return $.map(list, function (employee) {
+                                return {
+                                    employeeName: employee.Name,
+                                    employeeId: employee.Id
+                                };
+                            });
+                        }
+                    }
+                };
             }
 
             function manageCommunityAssignmentsViewModel() {
                 var self = this;
                 
                 self.allCommunityAssignments = ko.observableArray([]);
-                self.typeaheadSelectedEmployeeId = ko.observable('');
-                self.typeaheadSelectedEmployeeName = ko.observable('');
-                self.theLookups = employees;
             }
+
+
+            //Twitter typeahead setup
+            //KO using custom binding with typeahead.
+            ko.bindingHandlers.typeahead = {
+                init: function (element, valueAccessor, allBindingsAccessor, bindingViewModel) {
+                    var el = $(element);
+                    var options = ko.utils.unwrapObservable(valueAccessor());
+                    var allBindings = allBindingsAccessor();
+
+                    var data = new Bloodhound({
+                        datumTokenizer: Bloodhound.tokenizers.obj.whitespace(options.displayKey),
+                        queryTokenizer: Bloodhound.tokenizers.whitespace,
+                        limit: options.limit,
+                        remote: options.remote
+                    });
+
+                    // kicks off the loading/processing of data.
+                    data.initialize();
+                    
+                    el.attr("autocomplete", "off").typeahead({
+                        hint: true,
+                        highlight: true,
+                        minLength: 2
+                    },
+                    {
+                        name: options.name,
+                        displayKey: options.displayKey,
+                        // `ttAdapter` wraps the suggestion engine in an adapter that
+                        // is compatible with the typeahead jQuery plugin
+                        source: data.ttAdapter()
+                    }).bind("typeahead:selected typeahead:autocompleted", function (obj, datum) {
+                        //save currently selected value on line item vm property.
+                        bindingViewModel.typeaheadSelectedEmployeeId(datum.employeeId);
+                        bindingViewModel.typeaheadSelectedEmployeeName(datum.employeeName);
+                    }).blur(function () {
+                        var currentEmployeeName = $(this).val();
+
+                        //ensures values selected is a valid employee bc user selected or prefilled with typeahead.
+                        if ($.trim(currentEmployeeName) != '' && currentEmployeeName === bindingViewModel.typeaheadSelectedEmployeeName()) {
+                            bindingViewModel.employeeName(currentEmployeeName);
+                            bindingViewModel.employeeId(bindingViewModel.typeaheadSelectedEmployeeId());
+                            bindingViewModel.assignEmployeeToCommunity();
+                        }
+
+                        $(this).val('');
+                        bindingViewModel.assigneeEditing(false);
+                        bindingViewModel.typeaheadSelectedEmployeeId('');
+                        bindingViewModel.typeaheadSelectedEmployeeName('');
+                    });
+                }
+            };
+            
 
             var viewModel = new manageCommunityAssignmentsViewModel();
             ko.applyBindings(viewModel);
@@ -66,73 +137,7 @@
             _(persistedCommunityAssignmentsItemsViewModel).each(function (item) {
                 viewModel.allCommunityAssignments.push(new CommunityAssignmentsItemsViewModel(item));
             });
-
-
-            //Twitter typeahead setup
             
-            //Constructs the suggestion engine
-            var employees = new Bloodhound({
-                datumTokenizer: Bloodhound.tokenizers.obj.whitespace('employeeName'),
-                queryTokenizer: Bloodhound.tokenizers.whitespace,
-                limit: 20,
-                remote: {
-                    url: urls.QuickSearch.Employees + '?query=%QUERY',
-                    filter: function (list) {
-                        return $.map(list, function(employee) {
-                            return {
-                                employeeName: employee.Name,
-                                employeeId: employee.Id
-                            };
-                        });
-                    }
-                }
-            });
-
-            employees.initialize();
-            
-            $('#employeeList .typeahead').typeahead({
-                hint: true,
-                highlight: true,
-                minLength: 2,
-            },
-            {
-                name: 'employees',
-                displayKey: 'employeeName',
-                // `ttAdapter` wraps the suggestion engine in an adapter that
-                // is compatible with the typeahead jQuery plugin. used for Bloodhound.
-                source: employees.ttAdapter()
-            }).bind("typeahead:selected typeahead:autocompleted", function (obj, datum) {
-                //keep track of valid employee bc user selected or prefilled with typeahead.
-                viewModel.typeaheadSelectedEmployeeName = datum.employeeName;
-                viewModel.typeaheadSelectedEmployeeId = datum.employeeId;
-            }).blur(function () {
-                var currentEmployeeName = $(this).val();
-                
-                //ensures values selected is a valid employee bc user selected or prefilled with typeahead.
-                if ($.trim(currentEmployeeName) != '' && currentEmployeeName === viewModel.typeaheadSelectedEmployeeName) {
-                    setEmployeeLineDetails(this, currentEmployeeName, viewModel.typeaheadSelectedEmployeeId);
-                }
-
-                $('.typeahead').typeahead('val', '');
-                $(this).closest('#employeeSearch').val('');
-                resetEmployeeLineEdit(this);
-            });
-            
-            function setEmployeeLineDetails(currentElement, theEmployeeName, theEmployeeId) {
-                var communityRow = $(currentElement).closest('.communityDesc');
-                var arrayIndex = communityRow.attr('data-manage-assignment-line-item') - 1;
-
-                viewModel.allCommunityAssignments()[arrayIndex].selectedEmployeeName(theEmployeeName);
-                viewModel.allCommunityAssignments()[arrayIndex].selectedEmployeeId(theEmployeeId);
-                viewModel.allCommunityAssignments()[arrayIndex].assignEmployeeToCommunity();
-            }
-            
-            function resetEmployeeLineEdit(currentElement) {
-                var communityRow = $(currentElement).closest('.communityDesc');
-                var arrayIndex = communityRow.attr('data-manage-assignment-line-item') - 1;
-
-                viewModel.allCommunityAssignments()[arrayIndex].assigneeEditing(false);
-            }
         });
     });
 });
