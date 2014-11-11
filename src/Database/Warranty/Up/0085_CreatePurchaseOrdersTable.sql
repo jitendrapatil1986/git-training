7d62c6a4400287a5946cf4f1aade97bc8e3c88eb
Kiet Huynh <kiet.huynh@headspring.com>
2014-11-11 15:36:38 -0600

    BS-5253 - Create Purchase Order header and line tables.

---
+++ src/Database/Warranty/Up/0085_CreatePurchaseOrdersTable.sql
@@ -0,0 +1,35 @@
+
+CREATE TABLE [dbo].[PurchaseOrders](
+    [PurchaseOrderId] [uniqueidentifier] NOT NULL,
+    [VendorNumber] [varchar](8) NULL,
+	[VendorName] [varchar](255) NULL,
+	[Type] [varchar](50) NULL,
+	[DeliveryInstructions] [varchar](255) NULL,
+	[DeliveryDate] [datetime2](7) NULL,
+	[CostCode] [varchar](255) NULL,
+	[ObjectAccount] [varchar](255) NULL,
+	[ServiceCallLineItemId] [uniqueidentifier] NULL,
+    [JobNumber] [varchar](8) NULL,
+    [JdeIdentifier] [varchar](255) NULL,
+	[PurchaseOrderNote] [varchar](max) NULL,
+	[CreatedDate] [datetime2](7) NULL,
+	[CreatedBy] [varchar](255) NULL,
+	[UpdatedDate] [datetime2](7) NULL,
+	[UpdatedBy] [varchar](255) NULL,
+ CONSTRAINT [PK_PurchaseOrders] PRIMARY KEY CLUSTERED 
+(
+	[PurchaseOrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [ActiveData]
+) ON [ActiveData]
+
+GO
+
+ALTER TABLE [dbo].[PurchaseOrders] ADD  CONSTRAINT [DF_PurchaseOrders_PurchaseOrderId]  DEFAULT (newsequentialid()) FOR [PurchaseOrderId]
+GO
+
+ALTER TABLE [dbo].[PurchaseOrders]  WITH CHECK ADD  CONSTRAINT [FK_PurchaseOrders_ServiceCallLineItemId] FOREIGN KEY([ServiceCallLineItemId])
+REFERENCES [dbo].[ServiceCallLineItems] ([ServiceCallLineItemId])
+GO
+
+ALTER TABLE [dbo].[PurchaseOrders] CHECK CONSTRAINT [FK_PurchaseOrders_ServiceCallLineItemId]
+GO
---
+++ src/Database/Warranty/Up/0086_CreatePurchaseOrderLineItemsTable.sql
@@ -0,0 +1,32 @@
+
+CREATE TABLE [dbo].[PurchaseOrderLineItems](
+	[PurchaseOrderLineItemId] [uniqueidentifier] NOT NULL,
+	[PurchaseOrderId] [uniqueidentifier] NULL,
+	[LineNumber] [int] NULL,
+	[Quantity] [decimal](18, 2) NULL,
+	[UnitCost] [decimal](18, 2) NULL,
+	[Description] [varchar](4000) NULL,
+	[PurchaseOrderLineItemStatusId] [int] NULL,
+	[CreatedDate] [datetime2](7) NULL,
+	[CreatedBy] [varchar](255) NULL,
+	[UpdatedDate] [datetime2](7) NULL,
+	[UpdatedBy] [varchar](255) NULL,
+ CONSTRAINT [PK_PurchaseOrderLineItems] PRIMARY KEY CLUSTERED 
+(
+	[PurchaseOrderLineItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [ActiveData]
+) ON [ActiveData]
+
+GO
+
+ALTER TABLE [dbo].[PurchaseOrderLineItems] ADD  CONSTRAINT [DF_PurchaseOrderLineItems]  DEFAULT (newsequentialid()) FOR [PurchaseOrderLineItemId]
+GO
+
+ALTER TABLE [dbo].[PurchaseOrderLineItems]  WITH CHECK ADD  CONSTRAINT [FK_PurchaseOrderLineItems_PurchaseOrderId] FOREIGN KEY([PurchaseOrderId])
+REFERENCES [dbo].[PurchaseOrders] ([PurchaseOrderId])
+GO
+
+ALTER TABLE [dbo].[PurchaseOrderLineItems] CHECK CONSTRAINT [FK_PurchaseOrderLineItems_PurchaseOrderId]
+GO
+
+
