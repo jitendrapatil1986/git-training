49f3e3d93c76f7f1c8d0d0b0690db8e5c66d91fd
Adam Hopkins <adam.hopkins@headspring.com>
2014-11-19 16:29:50 -0600

    Handlers for Purchase Order Maximum changing in JDE

---
+++ src/Warranty.Server/Handlers/PurchaseOrders/CityPurchaseOrderMaxUpdatedHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace Warranty.Server.Handlers.PurchaseOrders
+{
+    using Accounting.Events.PurchaseOrders;
+    using Core.Entities;
+    using NPoco;
+    using NServiceBus;
+
+    public class CityPurchaseOrderMaxUpdatedHandler : IHandleMessages<CityPurchaseOrderMaxUpdated>
+    {
+        private readonly IDatabase _database;
+
+        public CityPurchaseOrderMaxUpdatedHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Handle(CityPurchaseOrderMaxUpdated message)
+        {
+            using (_database)
+            {
+                var existingCity = _database.Single<City>("SELECT * FROM Cities WHERE CityCode = @0", message.JDEId);
+                
+                existingCity.PurchaseOrderMaxAmount = message.MaxPurchaseOrderAmount;
+                _database.Update(existingCity);
+            }
+        }
+    }
+}
