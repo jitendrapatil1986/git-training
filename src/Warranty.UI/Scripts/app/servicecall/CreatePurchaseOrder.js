036b28b7f8224bdb26b885d5dbae84a42bdcde06
Kiet Huynh <kiet.huynh@headspring.com>
2014-12-03 13:45:49 -0600

    Update Delivery Date to only allow future dates.

--- src/Warranty.UI/Scripts/app/servicecall/CreatePurchaseOrder.js
+++ src/Warranty.UI/Scripts/app/servicecall/CreatePurchaseOrder.js
@@ -4,8 +4,14 @@
 
         require(['ko.validation'], function () {
             $(function () {
-
-                $(".datepicker-input").datepicker();
+                var nowTemp = new Date();
+                var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+                
+                $(".datepicker").datepicker({
+                    onRender: function (date) {
+                        return date.valueOf() < now.valueOf() ? 'disabled' : '';
+                    }
+                });
                 
                 function PurchaseOrderLineViewModel() {
                     var self = this;
