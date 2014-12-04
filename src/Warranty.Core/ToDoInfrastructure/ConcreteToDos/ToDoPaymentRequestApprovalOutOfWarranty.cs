7327df98a7f0d6f7d33b9c5d1aa400bc5b00e71d
Eduardo Ramos <eduardo@headspring.com>
2014-12-03 18:36:04 -0600

    Differentiate between payment types on ToDo widget dropdown

---
+++ src/Warranty.Core/ToDoInfrastructure/ConcreteToDos/ToDoPaymentRequestApprovalOutOfWarranty.cs
@@ -0,0 +1,18 @@
+﻿namespace Warranty.Core.ToDoInfrastructure.ConcreteTodos
+{
+    using Enumerations;
+    using Models;
+
+    public class ToDoPaymentRequestApprovalOutOfWarranty : ToDo<ToDoPaymentRequestApprovalOutOfWarrantyModel>
+    {
+        public override string ViewName
+        {
+            get { return "_ToDoPaymentRequestApprovalOutOfWarranty"; }
+        }
+
+        public override ToDoType Type
+        {
+            get { return ToDoType.PaymentRequestApprovalOutOfWarranty; }
+        }
+    }
+}
