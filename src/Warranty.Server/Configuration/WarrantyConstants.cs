e9eedd48a23c419ad2aae5efb041c7fc828dcdbc
Adam Hopkins <adam.hopkins@headspring.com>
2014-12-16 14:10:57 -0600

    material codes and labor codes are backwards.

--- src/Warranty.Server/Configuration/WarrantyConstants.cs
+++ src/Warranty.Server/Configuration/WarrantyConstants.cs
@@ -11,7 +11,7 @@
         public static readonly string DefaultPurchaseOrderType = "EO";
         public static readonly string DefaultWarrantyJobNumberSuffix = "9999";
 
-        public static readonly List<string> LaborObjectAccounts = new List<string> { "9425", "9435" };
-        public static readonly List<string> MaterialObjectAccounts = new List<string> {"9430", "9440"};
+        public static readonly List<string> LaborObjectAccounts = new List<string> { "9430", "9440"  };
+        public static readonly List<string> MaterialObjectAccounts = new List<string> { "9425", "9435" };
     }
 }
