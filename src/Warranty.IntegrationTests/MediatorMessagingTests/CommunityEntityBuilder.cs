f24a106b85dd35e77c6847b410a3ad53c1a930fa
Eduardo Ramos <eduardo@headspring.com>
2014-12-24 11:32:06 -0600

    Added misisng entity builder

---
+++ src/Warranty.IntegrationTests/MediatorMessagingTests/CommunityEntityBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NPoco;
+using Warranty.Core.Entities;
+
+namespace Warranty.IntegrationTests.MediatorMessagingTests
+{
+    public class CommunityEntityBuilder : EntityBuilder<Community>
+    {
+        public CommunityEntityBuilder(IDatabase database)
+            : base(database)
+        {
+        }
+
+        public override Community GetSaved(Action<Community> action)
+        {
+            var comNum = Guid.NewGuid().ToString("n").Substring(1, 4);
+
+            var entity = new Community
+                {
+                    CommunityNumber = comNum,
+                    CreatedBy = "test",
+                    CreatedDate = DateTime.UtcNow,
+                };
+
+            return Saved(entity, action);
+        }
+    }
+}
