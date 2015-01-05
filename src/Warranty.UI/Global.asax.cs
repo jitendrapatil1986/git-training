a237b0345e01965c348e5767e61ba5baee04594e
Eduardo Ramos <eduardo@headspring.com>
2015-01-05 14:07:52 -0600

    Add check to verify employee exists

--- src/Warranty.UI/Global.asax.cs
+++ src/Warranty.UI/Global.asax.cs
@@ -1,11 +1,18 @@
-﻿using Warranty.Core.Security;
-
-namespace Warranty.UI
+﻿namespace Warranty.UI
 {
     using Core.Initialization;
+    using Microsoft.Practices.ServiceLocation;
+    using Warranty.Core;
+    using Warranty.Core.Features.SessionStart;
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Session_Start()
+        {
+            var mediator = ServiceLocator.Current.GetInstance<IMediator>();
+            mediator.Send(new VerifyEmployeeExistsCommand());
+        }
+
         protected void Application_Start()
         {
             WarrantyWebsiteBootstrapper.Boostrap();
