ced74aecbede81bafbc4065734c82bf46c41d83a
Kiet Huynh <kiet.huynh@headspring.com>
2015-01-08 15:34:23 -0600

    Add model and query for WSR email notifications on service call completed.

---
+++ src/Warranty.Core/Features/ServiceCallToggleActions/ServiceCallCompleteWsrNotificationModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Warranty.Core.Features.ServiceCallToggleActions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ServiceCallCompleteWsrNotificationModel
+    {
+        public Guid ServiceCallId { get; set; }
+        public string ServiceCallNumber { get; set; }
+        public Guid WarrantyRepresentativeEmployeeId { get; set; }
+        public string WarrantyRepresentativeEmployeeEmail { get; set; }
+        public string HomeOwnerName { get; set; }
+        public string HomePhone { get; set; }
+        public string CommunityName { get; set; }
+        public string AddressLine { get; set; }
+        public string EmployeeNumber { get; set; }
+        public string Url { get; set; }
+        public List<string> Comments { get; set; }
+    }
+}
---
+++ src/Warranty.Core/Features/ServiceCallToggleActions/ServiceCallCompleteWsrNotificationQueryHandler.cs
@@ -0,0 +1,59 @@
+﻿namespace Warranty.Core.Features.ServiceCallToggleActions
+{
+    using NPoco;
+
+    public class ServiceCallCompleteWsrNotificationQueryHandler : IQueryHandler<ServiceCallCompleteWsrNotificationQuery, ServiceCallCompleteWsrNotificationModel>
+    {
+        private readonly IDatabase _database;
+
+        public ServiceCallCompleteWsrNotificationQueryHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public ServiceCallCompleteWsrNotificationModel Handle(ServiceCallCompleteWsrNotificationQuery query)
+        {
+            using (_database)
+            {
+                const string sql = @"SELECT ServiceCallId
+                                    , ServiceCallNumber
+                                    , WarrantyRepresentativeEmployeeId
+                                    , ho.HomeOwnerName
+                                    , ho.HomePhone
+                                    , c.CommunityName
+                                    , j.AddressLine
+                                    , e.EmployeeNumber
+                                  FROM ServiceCalls sc
+                                  INNER JOIN Jobs j
+                                    ON sc.JobId = j.JobId
+                                  INNER JOIN communities c
+                                    ON c.CommunityId = j.CommunityId
+                                  INNER JOIN HomeOwners ho
+                                    ON j.CurrentHomeOwnerId = ho.HomeOwnerId
+                                  LEFT OUTER JOIN Employees e
+                                    ON e.EmployeeId = sc.WarrantyRepresentativeEmployeeId
+                                  WHERE sc.ServiceCallId = @0";
+
+                var model = _database.SingleOrDefault<ServiceCallCompleteWsrNotificationModel>(sql, query.ServiceCallId);
+                model.WarrantyRepresentativeEmployeeEmail = GetEmployeeEmail(model.EmployeeNumber);
+
+                const string sqlcomments = @"SELECT ServiceCallNote
+                                             FROM ServiceCallNotes
+                                             WHERE ServiceCallId = @0";
+
+                model.Comments = _database.Fetch<string>(sqlcomments, query.ServiceCallId);
+
+                return model;
+            }
+        }
+
+        public string GetEmployeeEmail(string employeeNumber)
+        {
+            if (employeeNumber == null)
+                return null;
+
+            var queryEmail = new Common.Security.Queries.GetEmailByEmployeeNumberQuery();
+            return queryEmail.Execute(employeeNumber);
+        }
+    }
+}
