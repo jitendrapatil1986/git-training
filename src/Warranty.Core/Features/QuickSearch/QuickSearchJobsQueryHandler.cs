c0f88f5574cecb4018fcadf79c3fa4b8a40d2470
Eduardo Ramos <eduardo@headspring.com>
2015-01-08 14:53:28 -0600

    Changed job's quick search query to properly include all types of phone number entries

--- src/Warranty.Core/Features/QuickSearch/QuickSearchJobsQueryHandler.cs
+++ src/Warranty.Core/Features/QuickSearch/QuickSearchJobsQueryHandler.cs
@@ -29,7 +29,9 @@
                                 ON j.CommunityId = c.CommunityId
                                 INNER JOIN Cities m
                                 ON c.CityId = m.CityId
-                                WHERE CityCode IN ({0}) AND JobNumber+AddressLine+ISNULL(HomeOwnerName, '')+REPLACE(REPLACE(REPLACE(ISNULL(HomePhone, ''), ')', ''), '(', ''), ' ', '')+ISNULL(EmailAddress, '') LIKE '%'+@0+'%'
+                                WHERE CityCode IN ({0}) AND 
+                                JobNumber+AddressLine+ISNULL(HomeOwnerName, '')+ REPLACE(REPLACE(REPLACE(REPLACE(ISNULL(HomePhone, ''), ')', ''), '(', ''), ' ', ''),'-','') + ISNULL(EmailAddress, '')  LIKE '%'+ @0 +'%' OR 
+                                JobNumber+AddressLine+ISNULL(HomeOwnerName, '')+ REPLACE(REPLACE(REPLACE(REPLACE(ISNULL(HomePhone, ''), ')', ''), '(', ''), ' ', ''),'-','') + ISNULL(EmailAddress, '')  LIKE '%'+ REPLACE(REPLACE(REPLACE(REPLACE('%'+ @0 +'%', ')', ''), '(', ''), ' ', ''),'-','') +'%'
                                 ORDER BY HomeOwnerName";
 
             var result = _database.Fetch<QuickSearchJobModel>(string.Format(sqlTemplate, markets.CommaSeparateWrapWithSingleQuote()), query.Query);
