744b351eb8b358482684b28a0ab8554c2a634c66
Kiet Huynh <kiet.huynh@headspring.com>
2015-01-16 11:33:10 -0600

    Rename model and update command to also close out the call when the user verifies the homeowner signature.

--- src/Warranty.Core/Features/EditServiceCallStatus/VerifyHomeownerSignatureAndCloseCallCommand.cs
+++ src/Warranty.Core/Features/EditServiceCallStatus/VerifyHomeownerSignatureAndCloseCallCommand.cs
@@ -3,12 +3,12 @@
     using System;
     using Enumerations;
 
-    public class VerifyHomeownerSignatureServiceCallStatusModel
+    public class VerifyHomeownerSignatureAndCloseCallCommand : ICommand<VerifyHomeownerSignatureAndCloseCallModel>
     {
         public Guid ServiceCallId { get; set; }
         public ServiceCallStatus ServiceCallStatus { get; set; }
         public string HomeownerVerificationSignature { get; set; }
         public DateTime? HomeownerVerificationSignatureDate { get; set; }
-        public HomeownerVerificationType HomeownerVerificationType { get; set; }
+        public int HomeownerVerificationTypeId { get; set; }
     }
 }
