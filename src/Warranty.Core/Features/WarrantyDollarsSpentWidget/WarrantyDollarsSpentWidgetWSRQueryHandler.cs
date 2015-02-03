522c9d699e0601342584ad56934d97c839adba31
Kiet Huynh <kiet.huynh@headspring.com>
2015-02-03 13:58:09 -0600

    Update dashboard widgets for the warranty dollars spent calculation to only include warranty payments which have a status of paid and have a paid date which fall within 2 years of the home's closing date.

--- src/Warranty.Core/Features/WarrantyDollarsSpentWidget/WarrantyDollarsSpentWidgetWSRQueryHandler.cs
+++ src/Warranty.Core/Features/WarrantyDollarsSpentWidget/WarrantyDollarsSpentWidgetWSRQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿namespace Warranty.Core.Features.WarrantyDollarsSpentWidget
 {
+    using Enumerations;
     using Extensions;
     using NPoco;
     using Security;
@@ -48,12 +49,19 @@
                                         ON c.CommunityId = ca.CommunityId
                                         INNER JOIN Employees e
                                         ON ca.EmployeeId = e.EmployeeId
-                                    WHERE MONTH(p.CreatedDate) = MONTH(@0) AND YEAR(p.CreatedDate) = YEAR(@0) AND EmployeeNumber=@1";
+                                    WHERE MONTH(p.PaidDate) = MONTH(@0)
+                                    AND YEAR(p.PaidDate) = YEAR(@0)
+                                    AND p.PaidDate >= j.CloseDate
+                                    AND p.PaidDate <= DATEADD(yy, 2, j.CloseDate)
+                                    AND p.PaymentStatus = @2
+                                    AND p.PaidDate IS NOT NULL
+                                    AND CityCode IN ({0})
+                                    AND EmployeeNumber=@1";
 
                 var numberOfHomesThisMonth = _database.ExecuteScalar<int>(string.Format(sqlNumberOfHomes, user.Markets.CommaSeparateWrapWithSingleQuote()), -2, SystemTime.Today, user.EmployeeNumber);
-                var totalPaymentsThisMonth = _database.ExecuteScalar<decimal>(string.Format(sqlPayments, user.Markets.CommaSeparateWrapWithSingleQuote()), SystemTime.Today, user.EmployeeNumber);
+                var totalPaymentsThisMonth = _database.ExecuteScalar<decimal>(string.Format(sqlPayments, user.Markets.CommaSeparateWrapWithSingleQuote()), SystemTime.Today, user.EmployeeNumber, PaymentStatus.Paid.Value);
                 var numberOfHomesLastMonth = _database.ExecuteScalar<int>(string.Format(sqlNumberOfHomes, user.Markets.CommaSeparateWrapWithSingleQuote()), -2, SystemTime.Today.ToLastDay().AddMonths(-1), user.EmployeeNumber);
-                var totalPaymentsLastMonth = _database.ExecuteScalar<decimal>(string.Format(sqlPayments, user.Markets.CommaSeparateWrapWithSingleQuote()), SystemTime.Today.AddMonths(-1), user.EmployeeNumber);
+                var totalPaymentsLastMonth = _database.ExecuteScalar<decimal>(string.Format(sqlPayments, user.Markets.CommaSeparateWrapWithSingleQuote()), SystemTime.Today.AddMonths(-1), user.EmployeeNumber, PaymentStatus.Paid.Value);
 
                 return new WarrantyDollarsSpentWidgetModel
                            {
