d9cda738b1fb62947bbe677141135d169f82dc2e
Kiet Huynh <kiet.huynh@headspring.com>
2015-02-10 15:34:33 -0600

    Add feature for marking a service call line item as 'No Action'.

---
+++ src/Warranty.Core/Features/NoActionServiceCallLineItem/NoActionServiceCallLineItemCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Warranty.Core.Features.NoActionServiceCallLineItem
+{
+    using System;
+    using Enumerations;
+
+    public class NoActionServiceCallLineItemCommand : ICommand<ServiceCallLineItemStatus>
+    {
+        public Guid ServiceCallLineItemId { get; set; }
+    }
+}
