96c0c90e2e7ba04880502d1b316730d548655dd8
Deran Schilling <deran@headspring.com>
2015-02-17 13:38:26 -0600

    Add handlers for Division and Project name updates

---
+++ src/Warranty.Server/Handlers/Communities/ProjectNameUpdatedHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace Warranty.Server.Handlers.Communities
+{
+    using Accounting.Events;
+    using Core.Entities;
+    using NPoco;
+    using NServiceBus;
+
+    public class ProjectNameUpdatedHandler : IHandleMessages<ProjectNameUpdated>
+    {
+        private readonly IDatabase _database;
+
+        public ProjectNameUpdatedHandler(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public void Handle(ProjectNameUpdated message)
+        {
+            var project = _database.SingleOrDefault<Project>("WHERE ProjectNumber=@0", message.JDEId);
+            if (project == null)
+                return;
+
+            project.ProjectName = message.Name;
+            _database.Save<Project>(project);
+        }
+    }
+}
