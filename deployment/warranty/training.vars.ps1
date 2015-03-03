9b037a209d3c28eeaa436df8c668c6dc0b1d5131
Deran Schilling <deran@headspring.com>
2015-03-03 15:50:04 -0600

    Change training API calls to point to production API

--- deployment/warranty/training.vars.ps1
+++ deployment/warranty/training.vars.ps1
@@ -23,8 +23,8 @@ $errorReportingEmailAddresses = "deran@headspring.com, eduardo@headspring.com, k
 $errorReportingSubject = "Warranty Exception (Training)"
 
 $documentSharePath = "C:\temp\dwh_files\warranty"
-$surveyApiBaseUri = "http://surveytest.davidweekleyhomes.com/api/"
-$accountingApiBaseUri = "http://accountingtest.davidweekleyhomes.com/api/"
+$surveyApiBaseUri = "http://survey.davidweekleyhomes.com/api/"
+$accountingApiBaseUri = "http://accounting.davidweekleyhomes.com/api/"
 $jobServiceApiBaseUri = "http://jobservice.davidweekleyhomes.com"
 
 $accountingEndPointServer = "wkcorpapptrain1"
