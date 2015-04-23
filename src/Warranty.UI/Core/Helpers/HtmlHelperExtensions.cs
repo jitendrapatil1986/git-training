209389cefd6183e71d9f82c05770b3fc963dacb2
Farhan Alam <farhanalam99@gmail.com>
2015-04-23 17:22:24 -0500

    For @AaronValor

--- src/Warranty.UI/Core/Helpers/HtmlHelperExtensions.cs
+++ src/Warranty.UI/Core/Helpers/HtmlHelperExtensions.cs
@@ -1,13 +1,12 @@
-﻿using System.Web;
-using System.Web.WebPages;
-
-namespace Warranty.UI.Core.Helpers
+﻿namespace Warranty.UI.Core.Helpers
 {
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Web;
     using System.Web.Mvc;
     using System.Web.Mvc.Html;
+    using System.Web.WebPages;
     using Enumerations;
     using HtmlTags;
     using Initialization;
