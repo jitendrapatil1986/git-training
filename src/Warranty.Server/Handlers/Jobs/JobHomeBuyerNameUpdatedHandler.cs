9b4aa28f3b21ddaa04bacf4c326324a7f2324b2c
Farhan Alam <farhanalam99@gmail.com>
2015-07-07 10:45:44 -0500

    cc-3779: Adding homeowner if none existed before

--- src/Warranty.Server/Handlers/Jobs/JobHomeBuyerNameUpdatedHandler.cs
+++ src/Warranty.Server/Handlers/Jobs/JobHomeBuyerNameUpdatedHandler.cs
@@ -26,11 +26,13 @@ namespace Warranty.Server.Handlers.Jobs
                 if (job == null)
                     throw new Exception(string.Format("Job {0} not found in the Warranty database.", message.JDEId));
 
-                var homeOwner = _database.SingleById<HomeOwner>(job.CurrentHomeOwnerId);
+                HomeOwner homeOwner = null;
+                if (job.CurrentHomeOwnerId != null)
+                    homeOwner = _database.SingleById<HomeOwner>(job.CurrentHomeOwnerId);
 
                 if (homeOwner == null)
                 {
-                    homeOwner = new HomeOwner {HomeOwnerName = message.BuyerName, JobId = job.JobId};
+                    homeOwner = new HomeOwner { HomeOwnerName = message.BuyerName, JobId = job.JobId };
                 }
                 else
                 {
