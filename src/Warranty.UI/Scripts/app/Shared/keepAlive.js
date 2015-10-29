2c96761b47a0d959e73244cb58eee4e013ba3571
Farhan Alam <farhanalam99@gmail.com>
2015-10-29 09:15:12 -0500

    cc-4971: Expire session if val of milliSecondsUntilPromptAppears is negative or 0

--- src/Warranty.UI/Scripts/app/Shared/keepAlive.js
+++ src/Warranty.UI/Scripts/app/Shared/keepAlive.js
@@ -31,15 +31,16 @@
                 success: function (response) {
                     var sessionExpiresInMilliSecondsFromNow = response;
                     var tokenExpirationTime = moment().add(sessionExpiresInMilliSecondsFromNow, "ms");
-                    if (tokenExpirationTime <= now) {   /* Should trigger if the session wasn't renewed in time. 
+                    var promptCountDown = (2 * oneMinuteInMilliSeconds); //show countdown for 2 minutes. But prompt 3 minutes before actual expiration
+                    var milliSecondsUntilPromptAppears = sessionExpiresInMilliSecondsFromNow - promptCountDown - oneMinuteInMilliSeconds; //promptCountDown + (oneMinuteInMilliSeconds) = 3 minutes //3 minutes before expiration
+
+                    if (tokenExpirationTime <= now || milliSecondsUntilPromptAppears <= 0) {   /* Should trigger if the session wasn't renewed in time. 
                                                         Note: this "now" is not a true now. Round trip to server makes this outdated, 
                                                         but for calculations purposes, this should work.*/
                         expireSession();
                         return;
                     }
 
-                    var promptCountDown = (2 * oneMinuteInMilliSeconds); //show countdown for 2 minutes. But prompt 3 minutes before actual expiration
-                    var milliSecondsUntilPromptAppears = sessionExpiresInMilliSecondsFromNow - promptCountDown - oneMinuteInMilliSeconds; //promptCountDown + (oneMinuteInMilliSeconds) = 3 minutes //3 minutes before expiration
 
                     promptToContinueTimout = setTimeout(function () {
                         $(sessionTrackerContainer).off(sessionTrackingEvents);
