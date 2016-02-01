95c0f598040a1ed6ff0559774fa41953690d0e1e
Ronald Petrusek <dpetrusek@dwhomes.com>
2016-02-01 10:38:49 -0600

    Deleting

---
+++ src/Warranty.Server/Handlers/Jobs/BuyerTransferApprovedHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using TIPS.Events.JobEvents;
+
+namespace Warranty.Server.Handlers.Jobs
+{
+    public class BuyerTransferApprovedHandler : IHandleMessages<BuyerTransferApproved>
+    {
+        public BuyerTransferApprovedHandler()
+        {
+            
+        }
+    }
+}
