4c301d812e25ff215f43a9c98431d56613e78b6c
Ronald Petrusek <dpetrusek@dwhomes.com>
2016-02-17 15:58:31 -0600

    cc-6150 implementation of the showcase swing update approved handler

---
+++ src/Warranty.Server/Handlers/Jobs/ShowcaseSwingUpdateApprovedHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using log4net;
+using NServiceBus;
+using TIPS.Events.JobEvents;
+using Warranty.Core.Services;
+
+namespace Warranty.Server.Handlers.Jobs
+{
+    public class ShowcaseSwingUpdateApprovedHandler : IHandleMessages<ShowcaseSwingUpdateApproved>
+    {
+        private IJobService _jobService;
+        private readonly ILog _log = LogManager.GetLogger(typeof (ShowcaseSwingUpdateApprovedHandler));
+
+        public ShowcaseSwingUpdateApprovedHandler(IJobService jobService)
+        {
+            _jobService = jobService;
+        }
+
+        public void Handle(ShowcaseSwingUpdateApproved message)
+        {
+            if (string.IsNullOrWhiteSpace(message.JobNumber))
+                throw new InvalidOperationException("JobNumber is null or whitespace.");
+
+            var showcase = _jobService.GetJobByNumber(message.JobNumber);
+            if (showcase == null)
+                throw new InvalidOperationException("Showcase doesn't exist in Warranty. Can not update.");
+
+            showcase.Swing = message.Swing;
+            _jobService.Save(showcase);
+            _log.Info(string.Format("Updated showcase JobID '{0}'", showcase.JobId));
+        }
+    }
+}
