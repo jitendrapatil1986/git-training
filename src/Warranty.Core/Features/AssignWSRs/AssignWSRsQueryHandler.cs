bf0a8a03819dfadc7b59cad03372f77fe2d087c5
Ryan Hartzog <rhartzog@gmail.com>
2016-03-08 14:55:49 -0600

    cc-5706 restrict communties with no jobs

--- src/Warranty.Core/Features/AssignWSRs/AssignWSRsQueryHandler.cs
+++ src/Warranty.Core/Features/AssignWSRs/AssignWSRsQueryHandler.cs
@@ -34,6 +34,7 @@ namespace Warranty.Core.Features.AssignWSRs
                                             INNER JOIN Cities CT ON
                                             @0 LIKE '%' + CT.CityCode + '%'
                                             AND CT.CityId = C.CityId
+                                            WHERE (SELECT COUNT(jobnumber) FROM Jobs j WHERE j.CommunityId = a.CommunityId) > 0
                                             GROUP BY C.CommunityId
                                          ) a
                                          LEFT JOIN CommunityAssignments ca
