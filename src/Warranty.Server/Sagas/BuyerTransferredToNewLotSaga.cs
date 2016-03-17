747529982bf74608f77a54916e23c5f436ee84b8
David Savage <David Savage>
2016-03-17 13:31:31 -0500

    Remove Handler for BuyerTransferredToNewLot Complete Saga for BuyerTransferredToNewLot

--- src/Warranty.Server/Sagas/BuyerTransferredToNewLotSaga.cs
+++ src/Warranty.Server/Sagas/BuyerTransferredToNewLotSaga.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using AutoMapper;
 using NServiceBus;
 using NServiceBus.Saga;
+using TIPS.Commands.Requests;
+using TIPS.Commands.Responses;
 using TIPS.Events.JobEvents;
 using Warranty.Core.Entities;
 using Warranty.Core.Enumerations;
@@ -11,30 +14,35 @@ namespace Warranty.Server.Sagas
 {
     public class BuyerTransferredToNewLotSaga : Saga<BuyerTransferredToNewLotSagaData>,
         IAmStartedByMessages<BuyerTransferredToNewLot>,
-        IHandleMessages<BuyerTransferredToNewLotSaga_CleanExistingJob>,
+        IHandleMessages<BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner>,
         IHandleMessages<BuyerTransferredToNewLotSaga_EnsureNewJobExists>,
-        IHandleMessages<BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks>
-        //IHandleMessages<RequestJobSaleDetailsResponse>,
-        //IHandleMessages<CallbackFromTIPS_GetContact>
+        IHandleMessages<BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks>,
+        IHandleMessages<HomeBuyerDetailsResponse>,
+        IHandleMessages<JobSaleDetailsResponse>
     {
         private readonly IJobService _jobService;
         private readonly IHomeOwnerService _homeOwnerService;
         private readonly ITaskService _taskService;
+        private readonly IEmployeeService _employeeService;
+        private readonly ICommunityService _communityService;
+        private const string ENDPOINT_NAME = "Warranty.Server";
 
-        public BuyerTransferredToNewLotSaga(IJobService jobService, IHomeOwnerService homeOwnerService, ITaskService taskService)
+        public BuyerTransferredToNewLotSaga(IJobService jobService, IHomeOwnerService homeOwnerService, ITaskService taskService, IEmployeeService employeeService, ICommunityService communityService)
         {
             _jobService = jobService;
             _homeOwnerService = homeOwnerService;
             _taskService = taskService;
+            _employeeService = employeeService;
+            _communityService = communityService;
         }
 
         public override void ConfigureHowToFindSaga()
         {
-            ConfigureMapping<BuyerTransferredToNewLotSaga_CleanExistingJob>(x => x.ContactId).ToSaga(x => x.ContactId);
-            ConfigureMapping<BuyerTransferredToNewLotSaga_EnsureNewJobExists>(x => x.ContactId).ToSaga(x => x.ContactId);
-            ConfigureMapping<BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks>(x => x.ContactId).ToSaga(x => x.ContactId);
-            //ConfigureMapping<CallbackFromTIPS_GetContact>(x => x.ContactId).ToSaga(x => x.ContactId);
-            //ConfigureMapping<RequestJobSaleDetailsResponse>(x => x.ContactId).ToSaga(x => x.ContactId);
+            ConfigureMapping<BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner>(x => x.NewJobNumber).ToSaga(x => x.NewJobNumber);
+            ConfigureMapping<BuyerTransferredToNewLotSaga_EnsureNewJobExists>(x => x.NewJobNumber).ToSaga(x => x.NewJobNumber);
+            ConfigureMapping<BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks>(x => x.NewJobNumber).ToSaga(x => x.NewJobNumber);
+            ConfigureMapping<HomeBuyerDetailsResponse>(x => x.ContactId).ToSaga(x => x.ContactId);
+            ConfigureMapping<JobSaleDetailsResponse>(x => x.JobNumber).ToSaga(x => x.NewJobNumber);
         }
 
         public void Handle(BuyerTransferredToNewLot message)
@@ -43,50 +51,44 @@ namespace Warranty.Server.Sagas
             Data.PreviousJobNumber = message.PreviousJobNumber;
             Data.ContactId = message.ContactId;
 
-            var homeowner = _homeOwnerService.GetHomeOwnerByJobNumber(message.PreviousJobNumber);
-            if (homeowner == null)
+            var homeOwner = _homeOwnerService.GetHomeOwnerByJobNumber(message.PreviousJobNumber);
+            if (homeOwner == null)
             {
-                // Get Contact from TIPS
-                //Bus.Send(new GetHomeownerInformation(Data.ContactId));
+                // Get Homebuyer information from TIPS
+                Bus.Send(new RequestHomeBuyerDetails(Data.ContactId));
                 return;
             }
 
-            Bus.SendLocal(new BuyerTransferredToNewLotSaga_CleanExistingJob(Data.ContactId));
+            Data.HomeOwner = homeOwner;
+            Bus.SendLocal(new BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner(Data.NewJobNumber));
         }
 
-        //public void Handle(CallbackFromTIPS_GetHomeownerInformation message) // Response from TIPS
-        //{
-        //    var homeOwner = new HomeOwner
-        //    {
-        //        HomeOwnerId = Guid.NewGuid(),
-        //        CreatedBy = "Warranty.Server",
-        //        CreatedDate = DateTime.UtcNow,
-        //        HomeOwnerNumber = homeOwnerNumber,
-        //        UpdatedBy = "Warranty.Server"
-        //    };
-
-        //    Automapper.Map<HomeOwner>(message, homeOwner);
-
-        //    Data.NewHomeOwner = homeOwner;
-        //    Bus.SendLocal(new BuyerTransferredToNewLotSaga_EnsureNewJobExists
-        //    {
-        //        NewJobNumber = Data.NewJobNumber,
-        //        PreviousJobNumber = Data.PreviousJobNumber
-        //    });
-        //}
-
-        public void Handle(BuyerTransferredToNewLotSaga_CleanExistingJob message)
+        public void Handle(HomeBuyerDetailsResponse message)
         {
-            var homeowner = _homeOwnerService.GetHomeOwnerByJobNumber(Data.PreviousJobNumber);
+            var homeOwner = Mapper.Map<HomeOwner>(message);
+            homeOwner.HomeOwnerId = Guid.NewGuid();
+            homeOwner.HomeOwnerNumber = 1;
+            homeOwner.CreatedBy = ENDPOINT_NAME;
+            homeOwner.UpdatedBy = ENDPOINT_NAME;
+            homeOwner.CreatedDate = DateTime.UtcNow;
+            homeOwner.UpdatedDate = DateTime.UtcNow;
+
+            Data.HomeOwner = homeOwner;
+            Bus.SendLocal(new BuyerTransferredToNewLotSaga_EnsureNewJobExists(Data.NewJobNumber));
+        }
+
+        public void Handle(BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner message)
+        {
+            var homeOwner = _homeOwnerService.GetHomeOwnerByJobNumber(Data.PreviousJobNumber);
             var previousJob = _jobService.GetJobByNumber(Data.PreviousJobNumber);
 
-            _homeOwnerService.RemoveFromJob(homeowner, previousJob);
+            _homeOwnerService.RemoveFromJob(homeOwner, previousJob);
 
             _taskService.DeleteTask(previousJob.JobId, TaskType.JobStage3);
             _taskService.DeleteTask(previousJob.JobId, TaskType.JobStage10);
             _taskService.CreateTasks(previousJob.JobId);
 
-            Bus.SendLocal(new BuyerTransferredToNewLotSaga_EnsureNewJobExists(Data.ContactId));
+            Bus.SendLocal(new BuyerTransferredToNewLotSaga_EnsureNewJobExists(Data.NewJobNumber));
         }
 
         public void Handle(BuyerTransferredToNewLotSaga_EnsureNewJobExists message)
@@ -96,24 +98,33 @@ namespace Warranty.Server.Sagas
             if (newJob == null)
             {
                 // Get Sale from TIPS
-                // Bus.Send(new RequestJobSaleDetailsCommand(Data.NewJobNumber));
+                Bus.Send(new RequestJobSaleDetails { JobNumber = Data.NewJobNumber });
                 return;
             }
 
-            Bus.SendLocal(new BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(Data.ContactId));
+            Data.NewJob = newJob;
+            Bus.SendLocal(new BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(Data.NewJobNumber));
         }
 
-        //public void Handle(RequestJobSaleDetailsResponse message) // Response from TIPS
-        //{
-        //    var newJob = Automapper.Map<Job>(message);
-        //    Data.NewJob = _jobService.CreateJob(newJob);
+        public void Handle(JobSaleDetailsResponse message)
+        {
+            var newJob = Mapper.Map<Job>(message);
 
-        //    Bus.SendLocal(new BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(Data.ContactId));
-        //}
+            var builder = _employeeService.GetEmployeeByNumber(message.BuilderEmployeeID);
+            if (builder != null)
+                newJob.BuilderEmployeeId = builder.EmployeeId;
+
+            var community = _communityService.GetCommunityByNumber(message.CommunityNumber);
+            if(community != null)
+                newJob.CommunityId = community.CommunityId;
+
+            Data.NewJob = _jobService.CreateJob(newJob);
+            Bus.SendLocal(new BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(Data.NewJobNumber));
+        }
 
         public void Handle(BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks message)
         {
-            _homeOwnerService.AssignToJob(Data.NewHomeOwner, Data.NewJob);
+            _homeOwnerService.AssignToJob(Data.HomeOwner, Data.NewJob);
             _taskService.CreateTasks(Data.NewJob.JobId);
 
             MarkAsComplete();
@@ -122,37 +133,37 @@ namespace Warranty.Server.Sagas
 
     public class BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks : ICommand
     {
-        public Guid ContactId { get; set; }
+        public string NewJobNumber { get; set; }
 
         public BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks() { }
 
-        public BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(Guid contactId)
+        public BuyerTransferredToNewLotSaga_AssignHomeownerAndTasks(string newJobNumber)
         {
-            ContactId = contactId;
+            NewJobNumber = newJobNumber;
         }
     }
 
     public class BuyerTransferredToNewLotSaga_EnsureNewJobExists : ICommand
     {
-        public Guid ContactId { get; set; }
+        public string NewJobNumber { get; set; }
 
         public BuyerTransferredToNewLotSaga_EnsureNewJobExists() { }
 
-        public BuyerTransferredToNewLotSaga_EnsureNewJobExists(Guid contactId)
+        public BuyerTransferredToNewLotSaga_EnsureNewJobExists(string newJobNumber)
         {
-            ContactId = contactId;
+            NewJobNumber = newJobNumber;
         }
     }
 
-    public class BuyerTransferredToNewLotSaga_CleanExistingJob : ICommand
+    public class BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner : ICommand
     {
-        public Guid ContactId { get; set; }
+        public string NewJobNumber { get; set; }
 
-        public BuyerTransferredToNewLotSaga_CleanExistingJob() { }
+        public BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner() { }
 
-        public BuyerTransferredToNewLotSaga_CleanExistingJob(Guid contactId)
+        public BuyerTransferredToNewLotSaga_RemoveExistingHomeOwner(string newJobNumber)
         {
-            ContactId = contactId;
+            NewJobNumber = newJobNumber;
         }
     }
 
@@ -161,10 +172,13 @@ namespace Warranty.Server.Sagas
         public Guid Id { get; set; }
         public string Originator { get; set; }
         public string OriginalMessageId { get; set; }
+
         public string NewJobNumber { get; set; }
         public string PreviousJobNumber { get; set; }
+
+        [Unique]
         public Guid ContactId { get; set; }
-        public HomeOwner NewHomeOwner { get; set; }
+        public HomeOwner HomeOwner { get; set; }
         public Job NewJob { get; set; }
     }
 }
