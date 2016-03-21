f5aff051cc9ff43d1af5f40bea10195d6c91bfbf
David Savage <David Savage>
2016-03-21 12:07:50 -0500

    Change BuyerTransferToNewLotSaga to always request new job details from TIPS - this will keep Warranty up to date during the process

--- src/Warranty.Server.IntegrationTests/Sagas/BuyerTransferredToNewLot/JobSaleDetailsResponseTester.cs
+++ src/Warranty.Server.IntegrationTests/Sagas/BuyerTransferredToNewLot/JobSaleDetailsResponseTester.cs
@@ -16,7 +16,14 @@ namespace Warranty.Server.IntegrationTests.Sagas.BuyerTransferredToNewLot
     {
         private readonly int? Builder_Exists = 827838273;
         private const string Community_Exists = "278362763263";
-
+        private readonly Job ExistingJob = new Job
+        {
+            JobId = Guid.NewGuid(),
+            JobNumber = "72632763",
+            CreatedBy = "Someone",
+            CreatedDate = DateTime.UtcNow.AddDays(-2)
+        };
+        
         public BuyerTransferredToNewLotSagaData SagaData { get; set; }
         public BuyerTransferredToNewLotSaga Saga { get; set; }
 
@@ -25,8 +32,10 @@ namespace Warranty.Server.IntegrationTests.Sagas.BuyerTransferredToNewLot
         {
             Mapper.Initialize(m => m.AddProfile(new MappingProfile()));
 
-            var jobService = new Mock<IJobService>();
-            jobService.Setup(m => m.CreateJob(It.IsAny<Job>())).Returns(new Job()).Verifiable();
+            JobService = new Mock<IJobService>();
+            JobService.Setup(m => m.GetJobByNumber(ExistingJob.JobNumber)).Returns(ExistingJob).Verifiable();
+            JobService.Setup(m => m.CreateJob(It.IsAny<Job>())).Returns(new Job()).Verifiable();
+            JobService.Setup(m => m.Save(It.IsAny<Job>())).Verifiable();
 
             var homeOwnerService = new Mock<IHomeOwnerService>();
             var taskService = new Mock<ITaskService>();
@@ -51,12 +60,14 @@ namespace Warranty.Server.IntegrationTests.Sagas.BuyerTransferredToNewLot
 
             SagaData = new BuyerTransferredToNewLotSagaData();
 
-            Saga = new BuyerTransferredToNewLotSaga(jobService.Object, homeOwnerService.Object, taskService.Object, EmployeeService.Object, CommunityService.Object, log.Object)
+            Saga = new BuyerTransferredToNewLotSaga(JobService.Object, homeOwnerService.Object, taskService.Object, EmployeeService.Object, CommunityService.Object, log.Object)
             {
                 Bus = Bus,
                 Data = SagaData
             };
         }
+
+        public Mock<IJobService> JobService { get; set; }
 
         public Mock<ICommunityService> CommunityService { get; set; }
 
@@ -103,6 +114,65 @@ namespace Warranty.Server.IntegrationTests.Sagas.BuyerTransferredToNewLot
             SagaData.NewJob.ShouldNotBeNull();
 
             CommunityService.Verify(e => e.GetCommunityByNumber(Community_Exists), Times.Once);
+        }
+
+        [Test]
+        public void ShouldUpdateExistingJobWhenFound()
+        {
+            SagaData.NewJob = null;
+            JobService.ResetCalls();
+
+            var message = new JobSaleDetailsResponse
+            {
+                JobNumber = ExistingJob.JobNumber,
+                AddressCity = "Houston",
+                AddressLine1 = "123 Main St",
+                AddressStateAbbreviation = "TX",
+                AddressZipCode = "77571"
+            };
+
+            Saga.Handle(message);
+            SagaData.NewJob.ShouldNotBeNull();
+
+            JobService.Verify(m => m.GetJobByNumber(message.JobNumber), Times.Once);
+
+            var newJob = SagaData.NewJob;
+            newJob.ShouldNotBeNull();
+            newJob.JobId.ShouldEqual(ExistingJob.JobId);
+            newJob.CreatedBy.ShouldEqual(ExistingJob.CreatedBy);
+            newJob.CreatedDate.ShouldEqual(ExistingJob.CreatedDate);
+            newJob.JobNumber.ShouldEqual(ExistingJob.JobNumber);
+
+            newJob.AddressLine.ShouldEqual(message.AddressLine1);
+            newJob.City.ShouldEqual(message.AddressCity);
+            newJob.StateCode.ShouldEqual(message.AddressStateAbbreviation);
+            newJob.PostalCode.ShouldEqual(message.AddressZipCode);
+
+            JobService.Verify(m => m.Save(It.IsAny<Job>()), Times.Once);
+            JobService.Verify(m => m.CreateJob(It.IsAny<Job>()), Times.Never);
+        }
+
+        [Test]
+        public void ShouldCreateNewJobWhenNotFound()
+        {
+            SagaData.NewJob = null;
+            JobService.ResetCalls();
+
+            var message = new JobSaleDetailsResponse
+            {
+                JobNumber = "88273758"
+            };
+
+            Saga.Handle(message);
+            SagaData.NewJob.ShouldNotBeNull();
+
+            JobService.Verify(m => m.GetJobByNumber(message.JobNumber), Times.Once);
+
+            var newJob = SagaData.NewJob;
+            newJob.ShouldNotBeNull();
+            
+            JobService.Verify(m => m.Save(It.IsAny<Job>()), Times.Never);
+            JobService.Verify(m => m.CreateJob(It.IsAny<Job>()), Times.Once);
         }
     }
 }
