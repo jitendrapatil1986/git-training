cf425dc0a2d87b6664fb873d79733631ee7bdffe
David Savage <David Savage>
2016-03-24 14:49:41 -0500

    Fix issue where JobPlanUpdatedHandler threw exception when a job was not found

--- src/Warranty.Server/Handlers/Jobs/JobPlanUpdatedHandler.cs
+++ src/Warranty.Server/Handlers/Jobs/JobPlanUpdatedHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Accounting.Events.Job;
+using log4net;
 using NPoco;
 using NServiceBus;
 using Warranty.Core.Entities;
@@ -9,17 +10,25 @@ namespace Warranty.Server.Handlers.Jobs
     public class JobPlanUpdatedHandler : IHandleMessages<JobPlanUpdated>
     {
         private readonly IDatabase _database;
+        private readonly ILog _log;
 
-        public JobPlanUpdatedHandler(IDatabase database)
+        public JobPlanUpdatedHandler(IDatabase database, ILog log)
         {
             _database = database;
+            _log = log;
         }
 
         public void Handle(JobPlanUpdated message)
         {
             using (_database)
             {
-                var job = _database.SingleByJdeId<Job>(message.JDEId);
+                var job = _database.SingleOrDefaultByJdeId<Job>(message.JDEId);
+
+                if (job == null)
+                {
+                    _log.WarnFormat("Could not update JobPlan for job {0} because it does not exist locally", message.JDEId);
+                    return;
+                }
 
                 job.PlanNumber = message.Plan;
                 _database.Update(job);
