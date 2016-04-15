e3f1a35ac9ce52b6ddb02152fc8d0a87d14c4b48
Ryan Hartzog <rhartzog@gmail.com>
2016-04-15 15:44:12 -0500

    have a couple of tests, but not all of them

---
+++ src/Warranty.Tests/Entities/JobTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Warranty.Core.Entities;
+
+namespace Warranty.Tests.Entities
+{
+    [TestFixture]
+    public class JobTests
+    {
+
+        private readonly Job _job = new Job { CloseDate = new DateTime(2013, 4, 15) };
+
+        [Test]
+        public void Should_calculate_call_was_created_during_warranty_period()
+        {
+            var serviceCall = new ServiceCall { CreatedDate = new DateTime(2014, 04, 15) };
+
+        Assert.True(_job.WasCreatedDuringWarrantablePeriod(serviceCall));
+        }
+
+        [Test]
+        public void Should_calculate_call_was_not_created_during_warranty_period()
+        {
+            var serviceCall = new ServiceCall { CreatedDate = new DateTime(2016, 04, 15) };
+
+            Assert.False(_job.WasCreatedDuringWarrantablePeriod(serviceCall));
+        }
+    }
+}
