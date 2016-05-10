f3e5ee1f91240b9d6621975aad8ac7bbb481507c
David Savage <David Savage>
2016-05-10 14:22:11 -0500

    Update to clear data from prior runs

--- src/Warranty.HealthCheck/Handlers/ApprovedShowcasesHealthCheckSaga.cs
+++ src/Warranty.HealthCheck/Handlers/ApprovedShowcasesHealthCheckSaga.cs
@@ -13,6 +13,7 @@ namespace Warranty.HealthCheck.Handlers
 {
     public class ApprovedShowcasesHealthCheckSaga : Saga<ApprovedShowcasesHealthCheckSagaData>,
         IAmStartedByMessages<InitiateApprovedShowcasesHealthCheckSaga>,
+        IHandleMessages<ApprovedShowcasesHealthCheckSaga_ClearShowcasesFromTempTable>,
         IHandleMessages<ApprovedShowcasesHealthCheckSaga_GetApprovedShowcasesFromTips>,
         IHandleMessages<ApprovedShowcasesHealthCheckSaga_GetShowcasesFromWarranty>,
         IHandleMessages<ApprovedShowcasesHealthCheckSaga_CompareShowcasesFromTipsAndWarranty>
@@ -37,6 +38,13 @@ namespace Warranty.HealthCheck.Handlers
             }
 
             Data.Running = true;
+            Bus.SendLocal(new ApprovedShowcasesHealthCheckSaga_ClearShowcasesFromTempTable());
+        }
+        public void Handle(ApprovedShowcasesHealthCheckSaga_ClearShowcasesFromTempTable message)
+        {
+            _log.Info("Clearing any existing data from previous checks");
+            _mediator.Send(new ClearTempShowcasesTable());
+
             Bus.SendLocal(new ApprovedShowcasesHealthCheckSaga_GetApprovedShowcasesFromTips());
         }
 
@@ -58,6 +66,8 @@ namespace Warranty.HealthCheck.Handlers
 
         public void Handle(ApprovedShowcasesHealthCheckSaga_CompareShowcasesFromTipsAndWarranty message)
         {
+            _log.Info("Comparing data from TIPS for anything missing in Warranty");
+
             var tipsShowcases = _mediator.Send(new GetShowcases(Systems.TIPS));
             var warrantyShowcases = _mediator.Send(new GetShowcases(Systems.Warranty));
 
@@ -87,6 +97,8 @@ namespace Warranty.HealthCheck.Handlers
             MarkAsComplete();
         }
     }
+
+    public class ApprovedShowcasesHealthCheckSaga_ClearShowcasesFromTempTable : IBusCommand { }
 
     public class ApprovedShowcasesHealthCheckSaga_CompareShowcasesFromTipsAndWarranty : IBusCommand { }
 
