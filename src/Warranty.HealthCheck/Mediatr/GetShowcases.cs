66c592892f26bb2f29c22cbf85b534d65a456342
David Savage <David Savage>
2016-05-10 14:15:11 -0500

    Add HealthCheck to look for missing approved Showcases from Warranty

---
+++ src/Warranty.HealthCheck/Mediatr/GetShowcases.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using Warranty.HealthCheck.Data;
+using Warranty.HealthCheck.Models;
+
+namespace Warranty.HealthCheck.Mediatr
+{
+    public class GetShowcasesHandler : IRequestHandler<GetShowcases, IEnumerable<HEALTH_Showcase>>
+    {
+        private readonly IHealthCheckDatabase _healthCheckDatabase;
+
+        public GetShowcasesHandler(IHealthCheckDatabase healthCheckDatabase)
+        {
+            _healthCheckDatabase = healthCheckDatabase;
+        }
+
+        public IEnumerable<HEALTH_Showcase> Handle(GetShowcases message)
+        {
+            return _healthCheckDatabase.Fetch<HEALTH_Showcase>("WHERE System = @0", message.System);
+        }
+    }
+
+    public class GetShowcases : IRequest<IEnumerable<HEALTH_Showcase>>
+    {
+        public GetShowcases() { }
+
+        public GetShowcases(int system)
+        {
+            System = system;
+        }
+
+        public int System { get; set; }
+    }
+}
--- src/Warranty.HealthCheck/Models/HEALTH_Showcase.cs
+++ src/Warranty.HealthCheck/Models/HEALTH_Showcase.cs
@@ -1,6 +1,6 @@
 ﻿namespace Warranty.HealthCheck.Models
 {
-    public class HEALTH_SoldJob
+    public class HEALTH_Showcase
     {
         public string JobNumber { get; set; } 
         public int System { get; set; }
