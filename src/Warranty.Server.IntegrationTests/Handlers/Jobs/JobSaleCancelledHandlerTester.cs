ecd582f17c179ca220917dfdc13e3cc458b8e4e9
David Savage <David Savage>
2016-06-08 14:26:28 -0500

    Renaming JobStage10Complete to preserve Rodney's changes

--- src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobSaleCancelledHandlerTester.cs
+++ src/Warranty.Server.IntegrationTests/Handlers/Jobs/JobSaleCancelledHandlerTester.cs
@@ -76,7 +76,7 @@ namespace Warranty.Server.IntegrationTests.Handlers.Jobs
 
             _taskService.CreateTaskUnlessExists(job.JobId, TaskType.JobStage3);
             _taskService.CreateTaskUnlessExists(job.JobId, TaskType.JobStage7);
-            _taskService.CreateTaskUnlessExists(job.JobId, TaskType.JobStage10JobClosed);
+            _taskService.CreateTaskUnlessExists(job.JobId, TaskType.JobStage10Complete);
 
             var allTasks = _taskService.GetTasksByJobId(job.JobId);
             allTasks.Count.ShouldEqual(3);
