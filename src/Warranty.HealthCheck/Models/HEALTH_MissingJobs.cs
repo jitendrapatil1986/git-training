a53163876022bf29dcee65b7e0e07c437a0d5d9e
Coleman <rcoleman@corp.weekleyhomes.com>
2016-06-16 08:35:03 -0500

    cc-7176: Renamed the HealthCheck database table to store the job information to better align with the name of the corresponding saga.

--- src/Warranty.HealthCheck/Models/HEALTH_MissingJobs.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Warranty.HealthCheck.Models
-{
-    public class HEALTH_MissingJobs
-    {
-        public string JobNumber { get; set; }
-        public int System { get; set; }
-    }
-}
