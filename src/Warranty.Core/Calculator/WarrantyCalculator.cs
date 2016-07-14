4832ef498b9608a23e5b51ecbd7ae432b6e580d0
Coleman <rcoleman@corp.weekleyhomes.com>
2016-07-14 07:21:06 -0500

    cc-5336: Fixing indentation issues on SQL statements.

--- src/Warranty.Core/Calculator/WarrantyCalculator.cs
+++ src/Warranty.Core/Calculator/WarrantyCalculator.cs
@@ -165,22 +165,22 @@ namespace Warranty.Core.Calculator
             {
                 const string sql = @"SELECT 
                                         COUNT(*) AS TotalElements
-								        ,COUNT
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN 1
-										        ELSE NULL
-									        END
-								        ) AS TotalCalculableElements
-								        ,SUM
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
-										        ELSE 0
-									        END
-								        ) AS Amount
-								        ,MONTH(SC.CreatedDate) AS MonthNumber
-								        ,YEAR(SC.CreatedDate) AS YearNumber
+                                        ,COUNT
+                                        (
+	                                        CASE
+		                                        WHEN SC.SpecialProject = 0 THEN 1
+		                                        ELSE NULL
+	                                        END
+                                        ) AS TotalCalculableElements
+                                        ,SUM
+                                        (
+	                                        CASE
+		                                        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
+		                                        ELSE 0
+	                                        END
+                                        ) AS Amount
+                                        ,MONTH(SC.CreatedDate) AS MonthNumber
+                                        ,YEAR(SC.CreatedDate) AS YearNumber
                                     FROM dbo.Employees E
                                     INNER JOIN dbo.ServiceCalls SC
 	                                    ON SC.WarrantyRepresentativeEmployeeId = E.EmployeeID
@@ -208,23 +208,23 @@ namespace Warranty.Core.Calculator
             using (_database)
             {
                 const string sql = @"SELECT 
-	                                    COUNT(*) AS TotalElements
-	                                    ,COUNT
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN 1
-										        ELSE NULL
-									        END
-								        ) AS TotalCalculableElements
-								        ,SUM
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
-										        ELSE 0
-									        END
-								        ) AS Amount
-								        ,MONTH(SC.CreatedDate) AS MonthNumber
-								        ,YEAR(SC.CreatedDate) AS YearNumber
+                                        COUNT(*) AS TotalElements
+                                        ,COUNT
+                                        (
+                                        CASE
+	                                        WHEN SC.SpecialProject = 0 THEN 1
+	                                        ELSE NULL
+                                        END
+                                        ) AS TotalCalculableElements
+                                        ,SUM
+                                        (
+                                        CASE
+	                                        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
+	                                        ELSE 0
+                                        END
+                                        ) AS Amount
+                                        ,MONTH(SC.CreatedDate) AS MonthNumber
+                                        ,YEAR(SC.CreatedDate) AS YearNumber
                                     FROM dbo.Projects P
                                     INNER JOIN dbo.Communities C
 	                                    ON C.ProjectId = P.ProjectId
@@ -252,23 +252,23 @@ namespace Warranty.Core.Calculator
             using (_database)
             {
                 const string sql = @"SELECT 
-	                                    COUNT(*) AS TotalElements
-	                                    ,COUNT
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN 1
-										        ELSE NULL
-									        END
-								        ) AS TotalCalculableElements
-								        ,SUM
-								        (
-									        CASE
-										        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
-										        ELSE 0
-									        END
-								        ) AS Amount
-								        ,MONTH(SC.CreatedDate) AS MonthNumber
-								        ,YEAR(SC.CreatedDate) AS YearNumber
+                                        COUNT(*) AS TotalElements
+                                        ,COUNT
+                                        (
+	                                        CASE
+		                                        WHEN SC.SpecialProject = 0 THEN 1
+		                                        ELSE NULL
+	                                        END
+                                        ) AS TotalCalculableElements
+                                        ,SUM
+                                        (
+	                                        CASE
+		                                        WHEN SC.SpecialProject = 0 THEN DATEDIFF(DD, SC.CreatedDate, @0)
+		                                        ELSE 0
+	                                        END
+                                        ) AS Amount
+                                        ,MONTH(SC.CreatedDate) AS MonthNumber
+                                        ,YEAR(SC.CreatedDate) AS YearNumber
                                     FROM dbo.Divisions D
                                     INNER JOIN dbo.Communities C
 	                                    ON C.DivisionId = D.DivisionId
