50106a4b9d8e43f4e0ea91d7603f9246e21b9824
David Savage <David Savage>
2016-08-08 15:21:40 -0500

    Add Mediator mappings for Warranty.Server

--- src/Warranty.Server/Configuration/StructureMapBootstrapper.cs
+++ src/Warranty.Server/Configuration/StructureMapBootstrapper.cs
@@ -1,5 +1,6 @@
 ﻿using log4net;
 using StructureMap;
+using Warranty.Core;
 
 namespace Warranty.Server.Configuration
 {
@@ -9,11 +10,20 @@ namespace Warranty.Server.Configuration
         {
             container.Configure(cfg =>
             {
+                cfg.Scan(x =>
+                {
+                    x.AssemblyContainingType<IMediator>();
+                    x.AddAllTypesOf((typeof(IQueryHandler<,>)));
+                    x.AddAllTypesOf((typeof(ICommandHandler<>)));
+                    x.AddAllTypesOf((typeof(ICommandHandler<,>)));
+                    x.WithDefaultConventions();
+                });
                 cfg.For<ILog>().AlwaysUnique().Use(c =>
                 {
                     var parentType = c.ParentType ?? c.BuildStack.Current.ConcreteType;
                     return LogManager.GetLogger(parentType);
                 });
+
             });
         }
     }
