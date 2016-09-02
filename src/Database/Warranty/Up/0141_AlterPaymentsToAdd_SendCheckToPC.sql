11b8468042b811a1e36e7c81dfc24ce421ea914c
DWH\tklingsh <tklingshirn@dwhomes.com>
2016-09-02 14:24:25 -0500

    Save the PC name in the database to be able to display it later; show homeowner payment info after payment created

--- src/Database/Warranty/Up/0141_AlterPaymentsToAdd_SendCheckToPC.sql
+++ src/Database/Warranty/Up/0141_AlterPaymentsToAdd_SendCheckToPC.sql
@@ -1,1 +1,5 @@
-ALTER TABLE Payments ADD SendCheckToPC bit DEFAULT 0;
+if dbo.ColumnExists('Payments', 'SendCheckToPC') = 0
+begin
+    ALTER TABLE Payments ADD SendCheckToPC bit DEFAULT 0;
+end
+go
