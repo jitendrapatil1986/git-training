b96cb951239f802d634e843f7a08360bdb0dc6b3
jigargaglani <jgaglani@dwhomes.com>
2016-09-27 13:45:56 -0500

    cc-7722 JobClosedSaga_CloseJob message fails

--- src/Warranty.Core/Features/Job/CloseJobCommandHandler.cs
+++ src/Warranty.Core/Features/Job/CloseJobCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Warranty.Core.Enumerations;
+﻿using log4net;
+using Warranty.Core.Enumerations;
 using Warranty.Core.Services;
 
 namespace Warranty.Core.Features.Job
@@ -7,11 +8,15 @@ namespace Warranty.Core.Features.Job
     {
         private readonly IJobService _jobService;
         private readonly ITaskService _taskService;
+        private readonly IEmployeeService _employeeService;
+        private readonly ILog _log;
 
-        public CloseJobCommandHandler(IJobService jobService, ITaskService taskService)
+        public CloseJobCommandHandler(IJobService jobService, ITaskService taskService, IEmployeeService employeeService, ILog log)
         {
             _jobService = jobService;
             _taskService = taskService;
+            _employeeService = employeeService;
+            _log = log;
         }
 
         public void Handle(CloseJobCommand message)
@@ -19,6 +24,15 @@ namespace Warranty.Core.Features.Job
             var job = _jobService.GetJobById(message.JobId);
             job.CloseDate = message.CloseDate;
             _jobService.UpdateExistingJob(job);
+            _log.InfoFormat("{0} Job is marked close.", job.JobNumber);
+
+            var hasWsr = _employeeService.GetWsrByJobId(job.JobId) != null;
+
+            if (!hasWsr)
+            {
+                _log.ErrorFormat("{0} Job doesn't have a WSR assigned yet, so failed to create a task.", job.JobNumber);
+                return;
+            }
 
             _taskService.CreateTaskUnlessExists(job.JobId, TaskType.JobStage10JobClosed);
         }
