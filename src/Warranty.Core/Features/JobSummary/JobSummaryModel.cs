d3fd53ea0e280aa9ff61ba647795a5f49da0d84b
ibuchanan49 <ibuchanan@dwhomes.com>
2016-10-25 21:14:57 -0500

    cc-3577 added selection notes, attributes, date selected, and quantity to options tab

--- src/Warranty.Core/Features/JobSummary/JobSummaryModel.cs
+++ src/Warranty.Core/Features/JobSummary/JobSummaryModel.cs
@@ -145,6 +145,11 @@ namespace Warranty.Core.Features.JobSummary
         {
             public string OptionNumber { get; set; }
             public string OptionDescription { get; set; }
+            public string Notes { get; set; }
+            public IEnumerable<Attribute> Attributes { get; set; }
+            public DateTime DateSelected { get; set; }
+            public string Category { get; set; }
+            public int Quantity { get; set; }
         }
 
         public class JobServiceCall
@@ -234,6 +239,13 @@ namespace Warranty.Core.Features.JobSummary
             public string Description { get; set; }
             public TaskType TaskType { get; set; }
             public DateTime? CompletedDate { get; set; }
+        }
+
+        public class Attribute
+        {
+            public string Type { get; set; }
+            public string Value { get; set; }
+            public int DisplayOrder { get; set; }
         }
     }
 }
