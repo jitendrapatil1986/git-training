cc3955d7b5e0f905dd6eb73cf4f3d310ee211d48
ibuchanan49 <ibuchanan@dwhomes.com>
2016-10-28 12:23:26 -0500

    Updated mobile view and made cosmetic changes to the code based on pull request feedback

--- src/Warranty.Core/Features/JobSummary/JobSummaryQueryHandler.cs
+++ src/Warranty.Core/Features/JobSummary/JobSummaryQueryHandler.cs
@@ -71,7 +71,9 @@ namespace Warranty.Core.Features.JobSummary
                     OptionNumber = x.OptionNumber,
                     OptionDescription = x.Description,
                     Notes = x.Notes,
-                    Attributes = x.Attributes.OrderBy(y => y.Type.DisplayOrder).Select(y => new JobSummary.JobSummaryModel.Attribute { Type = y.Type.DisplayName, Value = y.Value}),
+                    Attributes = x.Attributes
+                                    .OrderBy(y => y.Type.DisplayOrder)
+                                    .Select(y => new JobSummary.JobSummaryModel.Attribute { Type = y.Type.DisplayName, Value = y.Value}),
                     RoomValue = x.Room.Value,
                     RoomDisplayName = x.Room.DisplayName
                 }).OrderBy(x => x.OptionDescription).ThenBy(x => x.OptionNumber);
