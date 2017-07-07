1945e32e5d44b3582bd0140ffb1af5256f34eab3
MedhaTrivedi <mtrivedi@loneoaksolutions.com>
2017-07-07 15:21:48 +0530

    cc-3182 - Remember expanded selection on call widget during session

--- src/Database/Warranty/Up/0143_Create_UserSettingsTable.sql
+++ src/Database/Warranty/Up/0143_Create_UserSettingsTable.sql
@@ -1,13 +1,17 @@
-﻿If dbo.TableExists('UserSettings') = 0
+﻿USE [WarrantyTest]
+If dbo.TableExists('UserSettings') = 0
 BEGIN
-CREATE TABLE UserSettings (
-    UserSettingsId UniqueIdentifier,
-    EmployeeId UniqueIdentifier,
-    ServiceCallWidgetSize int ,
-	UpdatedDate DATETIME,
+CREATE TABLE [dbo].[UserSettings] (
+    [UserSettingsId] [UniqueIdentifier] NOT NULL,
+    [EmployeeId] [UniqueIdentifier] NOT NULL,
+    [ServiceCallWidgetSize] [int] NULL,
+	[UpdatedDate] [DATETIME2] NULL,
+    [CreatedDate] [DATETIME2] NULL,
+    [CreatedBy] [VARCHAR](255) NULL,
+    [UpdatedBy] [VARCHAR](255) NULL,
     CONSTRAINT PK_UserSettingsId
         PRIMARY KEY (UserSettingsId),
-    CONSTRAINT FK_ServiceCallComments_ServiceCallId
+    CONSTRAINT [FK_ServiceCallComments_ServiceCallId]
         FOREIGN KEY (EmployeeId) REFERENCES Employees(EmployeeId)
 );
 END
