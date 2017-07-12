78598738ae280611ea32de2a0bb2f6afe34f1b32
MedhaTrivedi <mtrivedi@loneoaksolutions.com>
2017-07-12 20:22:49 +0530

    cc-3182 - Removed use statement and fixed whitespace

--- src/Database/Warranty/Up/0143_Create_UserSettingsTable.sql
+++ src/Database/Warranty/Up/0143_Create_UserSettingsTable.sql
@@ -1,12 +1,11 @@
-﻿USE [WarrantyTest]
-If dbo.TableExists('UserSettings') = 0
+﻿If dbo.TableExists('UserSettings') = 0
 BEGIN
 CREATE TABLE [dbo].[UserSettings] (
     [UserSettingsId] [UniqueIdentifier] NOT NULL,
     [EmployeeId] [UniqueIdentifier] NOT NULL,
     [ServiceCallWidgetSize] [int] NULL,
-	[UpdatedDate] [DATETIME2] NULL,
     [CreatedDate] [DATETIME2] NULL,
+    [UpdatedDate] [DATETIME2] NULL,
     [CreatedBy] [VARCHAR](255) NULL,
     [UpdatedBy] [VARCHAR](255) NULL,
     CONSTRAINT PK_UserSettingsId
