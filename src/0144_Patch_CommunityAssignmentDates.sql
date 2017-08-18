4cfc0a735db360e91b1d396b9e2b63f7149cdec5
MedhaTrivedi <mtrivedi@loneoaksolutions.com>
2017-08-18 16:46:11 +0530

    cc-9384 -  Call displays previous WSR after community reassignment (Patch Query)

---
+++ src/0144_Patch_CommunityAssignmentDates.sql
@@ -0,0 +1,11 @@
+﻿UPDATE CommunityAssignments 
+SET CreatedDate = Cast('01-01-1753' as datetime)
+Where CreatedDate Is Null
+
+UPDATE CommunityAssignments
+SET UpdatedDate = CreatedDate
+Where UpdatedDate Is Null
+
+UPDATE CommunityAssignments
+SET AssignmentDate = UpdatedDate
+Where AssignmentDate Is Null
